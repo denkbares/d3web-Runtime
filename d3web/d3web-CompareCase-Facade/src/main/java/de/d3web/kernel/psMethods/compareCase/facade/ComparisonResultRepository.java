78ba5f7bcae8f6ee76d6733fc63e59d7ef674cf2
Alex Legler <alex.legler@denkbares.com>
2009-09-12 22:16:34 +0000

    d3web-CompareCase-Facade MIGRATION
    	* NOTE: Added dash in name: "d3web CompareCase Facade" -> "d3web-CompareCase-Facade"
    	* pom.xml cleanup
    	* Adding license header
    	* Organizing imports

--- d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psMethods/compareCase/facade/ComparisonResultRepository.java
+++ d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psMethods/compareCase/facade/ComparisonResultRepository.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.psMethods.compareCase.facade;
 import java.util.*;
 
@@ -22,7 +42,7 @@ public class ComparisonResultRepository {
 
 	private List simpleResults = null;
 
-	private java.util.List sortedCases;
+	private List sortedCases;
 
 	private CompareMode compareMode = CompareMode.BOTH_FILL_UNKNOWN;
 
--- d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psMethods/compareCase/facade/DetailledResult.java
+++ d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psMethods/compareCase/facade/DetailledResult.java
@@ -1,12 +1,34 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.psMethods.compareCase.facade;
+
 import java.util.Iterator;
 import java.util.List;
 
 import de.d3web.kernel.domainModel.qasets.QContainer;
 import de.d3web.kernel.psMethods.compareCase.comparators.ComparatorResult;
+
 /**
- * Insert the type's description here.
- * Creation date: (22.08.01 01:45:07)
+ * Insert the type's description here. Creation date: (22.08.01 01:45:07)
+ * 
  * @author: Norman Brümmer
  */
 public class DetailledResult {
@@ -16,111 +38,98 @@ public class DetailledResult {
 	private double reachedPoints = 0;
 
 	private List comparatorResults = null;
-	
-/**
- * ContainerResult constructor comment.
- */
-public DetailledResult(QContainer cont, List results) {
-	super();
-	initialize(cont, results);
-}
 
-
-
-public void initialize(QContainer cont, List results) {
-	container = cont;
-	comparatorResults = results;
-	calcContainerResult();
-}
-
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 01:53:00)
- */
-private void calcContainerResult() {
-	Iterator iter = comparatorResults.iterator();
-	while (iter.hasNext()) {
-		ComparatorResult cres = (ComparatorResult) iter.next();
-		maxPoints += cres.getMaxPoints();
-		reachedPoints += cres.getReachedPoints();
+	/**
+	 * ContainerResult constructor comment.
+	 */
+	public DetailledResult(QContainer cont, List results) {
+		super();
+		initialize(cont, results);
 	}
 
-	similarity = reachedPoints / maxPoints;
-}
+	public void initialize(QContainer cont, List results) {
+		container = cont;
+		comparatorResults = results;
+		calcContainerResult();
+	}
 
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 01:53:00)
+	 */
+	private void calcContainerResult() {
+		Iterator iter = comparatorResults.iterator();
+		while (iter.hasNext()) {
+			ComparatorResult cres = (ComparatorResult) iter.next();
+			maxPoints += cres.getMaxPoints();
+			reachedPoints += cres.getReachedPoints();
+		}
 
-/**
- * Insert the method's description here.
- * Creation date: (23.08.2001 17:19:17)
- * @return java.lang.String
- */
-public String getContainerId() {
-	return container.getId();
-}
+		similarity = reachedPoints / maxPoints;
+	}
 
+	/**
+	 * Insert the method's description here. Creation date: (23.08.2001
+	 * 17:19:17)
+	 * 
+	 * @return java.lang.String
+	 */
+	public String getContainerId() {
+		return container.getId();
+	}
 
-/**
- * Insert the method's description here.
- * Creation date: (23.08.2001 17:19:17)
- * @return java.lang.String
- */
-public String getContainerName() {
-	return container.getText();
-}
+	/**
+	 * Insert the method's description here. Creation date: (23.08.2001
+	 * 17:19:17)
+	 * 
+	 * @return java.lang.String
+	 */
+	public String getContainerName() {
+		return container.getText();
+	}
 
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 01:52:24)
+	 * 
+	 * @return double
+	 */
+	public double getContainerSimilarity() {
+		return similarity;
+	}
 
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 01:52:24)
- * @return double
- */
-public double getContainerSimilarity() {
-	return similarity;
-}
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 03:25:53)
+	 * 
+	 * @return java.util.List
+	 */
+	public List getDetailledQuestionResults() {
+		return comparatorResults;
+	}
 
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 01:52:24)
+	 * 
+	 * @return double
+	 */
+	public double getMaxContainerPoints() {
+		return maxPoints;
+	}
 
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 03:25:53)
- * @return java.util.List
- */
-public List getDetailledQuestionResults() {
-	return comparatorResults;
-}
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 01:52:24)
+	 * 
+	 * @return double
+	 */
+	public double getReachedContainerPoints() {
+		return reachedPoints;
+	}
 
-
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 01:52:24)
- * @return double
- */
-public double getMaxContainerPoints() {
-	return maxPoints;
-}
-
-
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 01:52:24)
- * @return double
- */
-public double getReachedContainerPoints() {
-	return reachedPoints;
-}
-
-
-/**
- * Insert the method's description here.
- * Creation date: (22.08.01 17:16:13)
- * @return java.lang.String
- */
-public String toString() {
-	return container.getId()
-		+ ": "
-		+ reachedPoints
-		+ " = "
-		+ similarity
-		+ " * "
-		+ maxPoints + "::::" + comparatorResults;
-}
+	/**
+	 * Insert the method's description here. Creation date: (22.08.01 17:16:13)
+	 * 
+	 * @return java.lang.String
+	 */
+	public String toString() {
+		return container.getId() + ": " + reachedPoints + " = " + similarity
+				+ " * " + maxPoints + "::::" + comparatorResults;
+	}
 }
