77327f2e1dbeb73277b66b2999c3941669a9bf54
Alex Legler <alex.legler@denkbares.com>
2009-09-12 22:01:22 +0000

    d3web-SharedKnowledge MIGRATION
    	* pom.xml cleanup
    	* Adding license header

--- d3web/d3web-SharedKnowledge/src/main/java/de/d3web/kernel/psMethods/shared/PSMethodShared.java
+++ d3web/d3web-SharedKnowledge/src/main/java/de/d3web/kernel/psMethods/shared/PSMethodShared.java
@@ -1,6 +1,30 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.psMethods.shared;
+import java.util.Collection;
+
+import de.d3web.kernel.XPSCase;
 import de.d3web.kernel.psMethods.MethodKind;
 import de.d3web.kernel.psMethods.PSMethodAdapter;
+import de.d3web.kernel.psMethods.PropagationEntry;
 /**
  * psmethod for shared knowledge
  * Creation date: (03.08.2001 16:30:13)
@@ -11,4 +35,8 @@ public class PSMethodShared extends PSMethodAdapter {
 	public static MethodKind SHARED_WEIGHT = new MethodKind("SHARED_WEIGHT");
 	public static MethodKind SHARED_LOCAL_WEIGHT = new MethodKind("SHARED_LOCAL_WEIGHT");
 	public static MethodKind SHARED_ABNORMALITY = new MethodKind("SHARED_ABNORMALITY");
+
+	@Override
+	public void propagate(XPSCase theCase, Collection<PropagationEntry> changes) {
+	}
 }
