c7ee4b4128ca2fc5bd95e265cfc29a79eee8b4b4
Alex Legler <alex.legler@denkbares.com>
2009-09-12 22:42:04 +0000

    d3web-Verbalizer MIGRATION
    	* NOTE: Tests are broken and not migrated
    	* pom.xml cleanup
    	* Adding license header
    	* Organizing imports

--- d3web/d3web-Verbalizer/src/main/java/de/d3web/kernel/verbalizer/CondVerbalization.java
+++ d3web/d3web-Verbalizer/src/main/java/de/d3web/kernel/verbalizer/CondVerbalization.java
@@ -1,15 +1,40 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.verbalizer;
 
-import de.d3web.kernel.domainModel.ruleCondition.AbstractCondition;
-
-/**
- * Just a shared name for the classes NonTerminalCondVerbalization and
- * TerminalCondVerbalization and a wrapper for the return of the methods like
- * createConditionVerbalization(AbstractCondition absCondition)
- * in the ConditionVerbalizer which can return either 
- * TerminalCondVerbalizations or NonTerminalCondVerbalizations.
- * 
- * @author astriffler
- *
- */
-public abstract class CondVerbalization {}
+public class CondVerbalization {
+	
+	protected String operator;
+	protected String originalClass;
+	
+	protected CondVerbalization(String operator, String originalClass) {
+		this.operator = operator;
+		this.originalClass = originalClass;
+	}
+	
+	public String getOriginalClass() {
+		return originalClass;
+	}
+	
+	public String getOperator() {
+		return operator;
+	}
+}
--- d3web/d3web-Verbalizer/src/main/java/de/d3web/kernel/verbalizer/NonTerminalCondVerbalization.java
+++ d3web/d3web-Verbalizer/src/main/java/de/d3web/kernel/verbalizer/NonTerminalCondVerbalization.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ *                    Computer Science VI, University of Wuerzburg
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package de.d3web.kernel.verbalizer;
 
 import java.util.ArrayList;
@@ -5,26 +25,11 @@ import java.util.List;
 
 public class NonTerminalCondVerbalization extends CondVerbalization {
 	
-	private String operator;
 	private List<CondVerbalization> condVerbs = new ArrayList<CondVerbalization>();
-	private String originalClass;
 	
 	public NonTerminalCondVerbalization(List<CondVerbalization> condVerbs, String operator, String originalClass) {
+		super(operator, originalClass);
 		this.condVerbs = condVerbs;
-		this.operator = operator;
-		this.originalClass = originalClass;
-	}
-	
-	public String getOriginalClass() {
-		return originalClass;
-	}
-
-	public void setOriginalClass(String originalClass) {
-		this.originalClass = originalClass;
-	}
-	
-	public String getOperator() {
-		return operator;
 	}
 	
 	public List<CondVerbalization> getCondVerbalizations() {
