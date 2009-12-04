0638ab7b439403d2c3ef28bb1006824387b12cc0
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2009-12-04 12:46:14 +0000

    added '/' befor 'test.jar'

--- d3web/d3web-Persistence/src/test/java/de/d3web/persistence/tests/XCLModelTest.java
+++ d3web/d3web-Persistence/src/test/java/de/d3web/persistence/tests/XCLModelTest.java
@@ -93,7 +93,7 @@ public class XCLModelTest extends TestCase{
 		PersistenceManager pm=PersistenceManager.getInstance();		
 		pm.addPersistenceHandler(xmph);				
 		// URL url= new URL("file:/tmp/test.jar");
-		URL url= new URL("file://"+System.getProperty("java.io.tmpdir")+"test.jar");
+		URL url= new URL("file://"+System.getProperty("java.io.tmpdir")+"/test.jar");
 		pm.save(k,url);									
 		KnowledgeBase k2=pm.load(url);
 		Collection<KnowledgeSlice> col=k2.getAllKnowledgeSlicesFor(PSMethodXCL.class);
