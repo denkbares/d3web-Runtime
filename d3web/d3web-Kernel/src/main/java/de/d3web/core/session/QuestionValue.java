3ab206c702e5b21ab9d39553118cb5572ac3991d
Joachim Baumeister <joachim.baumeister@denkbares.com>
2010-05-10 15:16:42 +0000

    More code on the new implementation of a interview management in the d3web core. The interview management is "form-based" by now. So, you can ask session and interview management, respectively, to give the next "form" to be presented in a dialog. The "one question per form" strategy is already implemented.

---
+++ d3web/d3web-Kernel/src/main/java/de/d3web/core/session/QuestionValue.java
@@ -0,0 +1,13 @@
+package de.d3web.core.session;
+
+import de.d3web.core.knowledge.terminology.Question;
+
+/**
+ * This is a marker interface in order to identify that a 
+ * value is assigned to a {@link Question} instance.
+ * 
+ * @author joba
+ */
+public interface QuestionValue extends Value {
+
+}
