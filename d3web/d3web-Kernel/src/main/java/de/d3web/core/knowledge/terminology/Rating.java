4b7a1d1fa511a5895fde0955b19093612ce67456
Markus Friedrich <markus.friedrich@denkbares.com>
2010-05-17 10:14:58 +0000

    * DiagnosisState renamed to Rating * D3WebSession renamed to DefaultSession * Session.getSolutions(State) moved to Blackboard * Blackboard renamed to DefaultBlackboard * Interface Blackboard created * notification of D3webKnowledgeServiceSession reintroduced

---
+++ d3web/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/Rating.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
+ * Computer Science VI, University of Wuerzburg
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+
+package de.d3web.core.knowledge.terminology;
+
+import java.util.Comparator;
+
+import de.d3web.core.session.Value;
+import de.d3web.core.session.values.UndefinedValue;
+
+/**
+ * Stores the state of a {@link Solution} instance in context to a
+ * problem-solving method. The state is computed with respect to the values of
+ * the problem-solver actively contributing to the solution's state.
+ * 
+ * @author joba, Christian Betz
+ * @see Solution
+ * @see ScoreRating
+ */
+public class Rating implements Value {
+
+	/**
+	 * The four possible states of a {@link Solution}:
+	 * <UL>
+	 * <li>ESTABLISHED: categorically derived
+	 * <li>SUGGESTED: possibly derived
+	 * <li>UNCLEAR: no exact state, similar to {@link UndefinedValue} for
+	 * {@link Question}
+	 * <li>EXCLUDED: categorically derived as no possible solution
+	 * </UL>
+	 * 
+	 * @author joba (denkbares GmbH)
+	 * @created 15.04.2010
+	 */
+	public enum State {
+		EXCLUDED, UNCLEAR, SUGGESTED, ESTABLISHED;
+	}
+
+	private final State state;
+
+	/**
+	 * Creates a new solution state based on the specified {@link String} value.
+	 * The {@link String} value is case insensitive for backward compatibility.
+	 * 
+	 * @param statename the name of the solution state
+	 */
+	public Rating(String statename) {
+		this(State.valueOf(statename.toUpperCase()));
+	}
+
+	/**
+	 * Creates a new solution state instance based on the specified
+	 * {@link State} instance.
+	 * 
+	 * @param state the specified state instance
+	 */
+	public Rating(State state) {
+		if (state == null) throw new NullPointerException();
+		this.state = state;
+	}
+
+	/**
+	 * Returns the name of this {@link State} instance.
+	 * 
+	 * @return the name of this state instance
+	 */
+	public String getName() {
+		return this.state.name();
+	}
+
+	/**
+	 * Returns the current {@link State} of this instance.
+	 * 
+	 * @return the current state
+	 */
+	public State getState() {
+		return state;
+	}
+
+	/**
+	 * Compares the specified {@link State} instance with the {@link State}
+	 * instance contained in this instance.
+	 * 
+	 * @param state the specified state to be compared
+	 * @return true, if both states are equal; false otherwise
+	 */
+	public boolean hasState(State state) {
+		return this.state.equals(state);
+	}
+
+	/**
+	 * Returns whether the solution rated by this rating is relevant to the user
+	 * as a possible fault. This means that the rating is suggested or
+	 * established.
+	 * 
+	 * @return true, if the solution has the state suggested or established
+	 */
+	public boolean isRelevant() {
+		return hasState(State.SUGGESTED) || hasState(State.ESTABLISHED);
+	}
+
+	/**
+	 * Compares this rating with the specified rating for equality.
+	 * <p>
+	 * <b>Note:</b> any sub-class this method may compare additional (more
+	 * detailed) information about the rating, instead of the rating state only,
+	 * such as like heuristic score of the rating. Therefore two ratings with
+	 * the same state may be considered to be unequal (!).
+	 * <p>
+	 * This method uses the {@link #compareTo(Rating)} method to check
+	 * for equality, therefore subclasses usually have no need to overwrite this
+	 * method.
+	 * 
+	 * @param other the other {@link Rating} to be compared
+	 * @return true, when both states are equal; false otherwise
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other == null) return false;
+		if (!(other instanceof Rating)) return false;
+		return this.state.equals(((Rating) other).state);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.state.hashCode();
+	}
+
+	/**
+	 * Compares this rating with the specified rating for order. Returns a
+	 * negative integer, zero, or a positive integer as this object is less
+	 * than, equal to, or greater than the specified rating.
+	 * <p>
+	 * <b>Note:</b> any sub-class this method may compare additional (more
+	 * detailed) information about the rating, instead of the rating state only,
+	 * such as like heuristic score of the rating. Therefore two ratings with
+	 * the same state may be considered to be unequal.
+	 * <p>
+	 * If a subclass overwrites this method it must (!) call this method if the
+	 * state is compared to a instance with its class being identical to {@link
+	 * DiagnosisState#class}. An overwriting method shall call this method if
+	 * the state is compared to an instance of a different subclass.
+	 * 
+	 * @param other another {@link Rating}
+	 * @return the comparison result according to the {@link Comparator}
+	 *         definition
+	 */
+	@Override
+	public int compareTo(Value other) {
+		if (other instanceof Rating) {
+			return this.state.ordinal() - ((Rating) other).state.ordinal();
+		}
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+
+	@Override
+	public Object getValue() {
+		return getState();
+	}
+
+}
---
+++ d3web/d3web-Kernel/src/main/java/de/d3web/core/session/blackboard/DefaultBlackboard.java
@@ -0,0 +1,276 @@
+package de.d3web.core.session.blackboard;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import de.d3web.core.inference.PSMethod;
+import de.d3web.core.inference.PropagationContoller;
+import de.d3web.core.inference.PropagationEntry;
+import de.d3web.core.knowledge.Indication;
+import de.d3web.core.knowledge.InterviewObject;
+import de.d3web.core.knowledge.TerminologyObject;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionOC;
+import de.d3web.core.knowledge.terminology.Rating;
+import de.d3web.core.knowledge.terminology.Solution;
+import de.d3web.core.knowledge.terminology.info.Num2ChoiceSchema;
+import de.d3web.core.session.DefaultSession;
+import de.d3web.core.session.Session;
+import de.d3web.core.session.Value;
+import de.d3web.core.session.values.NumValue;
+import de.d3web.core.session.values.UndefinedValue;
+
+/**
+ * The Blackboard manages all dynamic values created within the case and
+ * propagated throughout the inference system.
+ * 
+ * @author volker_belli
+ * 
+ */
+public class DefaultBlackboard implements Blackboard {
+
+	private final DefaultSession session;
+	private final FactStorage valueStorage;
+	private final FactStorage interviewStorage;
+
+	// TODO: also manage the SessionObjects here
+
+	/**
+	 * Creates a new Blackboard for the specified xps session.
+	 * 
+	 * @param session the session the blackboard is created for
+	 */
+	public DefaultBlackboard(DefaultSession session) {
+		this.session = session;
+		this.valueStorage = new FactStorage(this);
+		this.interviewStorage = new FactStorage(this);
+	}
+
+	@Override
+	public Session getSession() {
+		return session;
+	}
+
+	@Override
+	public void addValueFact(Fact fact) {
+		Value oldValue;
+		TerminologyObject terminologyObject = fact.getTerminologyObject();
+		if (terminologyObject instanceof Solution) {
+			oldValue = getState((Solution) terminologyObject);
+		}
+		else if (terminologyObject instanceof Question) {
+			oldValue = getValue((Question) terminologyObject);
+		}
+		else {
+			oldValue = getValueFact(terminologyObject).getValue();
+		}
+		this.valueStorage.add(fact);
+		Fact newFact = getValueFact(terminologyObject);
+		Value newValue = newFact.getValue();
+		if (newValue != oldValue) {
+			PropagationContoller propagationContoller = session.getPropagationContoller();
+			propagationContoller.openPropagation();
+			propagationContoller.propagate(terminologyObject,
+					oldValue, newValue);
+			propagationContoller.commitPropagation();
+			session.notifyListeners(terminologyObject);
+		}
+	}
+
+	@Override
+	public void removeValueFact(Fact fact) {
+		this.valueStorage.remove(fact);
+	}
+
+	/**
+	 * Removes all value facts with the specified source from this blackboard
+	 * for the specified terminology object. If no such fact exists in the
+	 * blackboard, this method has no effect.
+	 * 
+	 * @param termObject the terminology object to remove the value facts from
+	 * @param source the fact source to be removed
+	 */
+	public void removeValueFact(TerminologyObject terminologyObject, Object source) {
+		this.valueStorage.remove(terminologyObject, source);
+	}
+
+	@Override
+	public Fact getValueFact(TerminologyObject terminologyObject) {
+		return this.valueStorage.getAggregator(terminologyObject).getMergedFact();
+	}
+
+	@Override
+	public Collection<TerminologyObject> getValuedObjects() {
+		return Collections.unmodifiableCollection(
+				this.valueStorage.getValuedObjects());
+	}
+
+	@Override
+	public Collection<Question> getValuedQuestions() {
+		Collection<Question> result = new LinkedList<Question>();
+		for (TerminologyObject object : getValuedObjects()) {
+			if (object instanceof Question) {
+				result.add((Question) object);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public Collection<Solution> getValuedSolutions() {
+		Collection<Solution> result = new LinkedList<Solution>();
+		for (TerminologyObject object : getValuedObjects()) {
+			if (object instanceof Solution) {
+				result.add((Solution) object);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public void addInterviewFact(Fact fact) {
+		// Besides adding the new fact to the interview management, we also do
+		// the notification
+		// of this new indication fact: this notification may be removed due to
+		// Session/Blackboard
+		// refactoring, i.e., when the notification is done at an upper place
+
+		InterviewObject factObject = (InterviewObject) fact.getTerminologyObject();
+
+		Value oldValue = getIndication(factObject);
+
+		this.interviewStorage.add(fact);
+
+		Value newValue = getIndication(factObject);
+		propagateIndicationChange(fact.getTerminologyObject(), oldValue, newValue);
+	}
+
+	@Override
+	public void removeInterviewFact(Fact fact) {
+		// Besides removing the fact to the interview management, we also do the
+		// notification
+		// of this deletion: this notification may be removed due to
+		// Session/Blackboard
+		// refactoring, i.e., when the notification is done at an upper place
+		InterviewObject factObject = (InterviewObject) fact.getTerminologyObject();
+		Value oldValue = getIndication(factObject);
+
+		this.interviewStorage.remove(fact);
+
+		Value newValue = getIndication(factObject);
+		propagateIndicationChange(factObject, oldValue, newValue);
+	}
+
+	@Override
+	public void removeInterviewFact(TerminologyObject terminologyObject, Object source) {
+		// Besides removing the fact to the interview management, we also do the
+		// notification
+		// of this deletion: this notification may be removed due to
+		// Session/Blackboard
+		// refactoring, i.e., when the notification is done at an upper place
+
+		InterviewObject factObject = (InterviewObject) terminologyObject;
+		Value oldValue = getIndication(factObject);
+
+		this.interviewStorage.remove(terminologyObject, source);
+
+		Value newValue = getIndication(factObject);
+		propagateIndicationChange(terminologyObject, oldValue, newValue);
+	}
+
+	private void propagateIndicationChange(TerminologyObject interviewObject, Value oldValue,
+			Value newValue) {
+		PropagationEntry entry = new PropagationEntry(interviewObject, oldValue,
+				newValue);
+		session.getInterviewManager().notifyFactChange(entry);
+	}
+
+	@Override
+	public void removeInterviewFacts(TerminologyObject terminologyObject) {
+		this.interviewStorage.remove(terminologyObject);
+	}
+
+	@Override
+	public Fact getInterviewFact(TerminologyObject terminologyObject) {
+		return this.interviewStorage.getAggregator(terminologyObject).getMergedFact();
+	}
+
+	@Override
+	public Collection<TerminologyObject> getInterviewObjects() {
+		return Collections.unmodifiableCollection(
+				this.interviewStorage.getValuedObjects());
+	}
+
+	@Override
+	public Rating getState(Solution solution) {
+		Fact valueFact = getValueFact(solution);
+		if (valueFact != null) {
+			return (Rating) valueFact.getValue();
+		}
+		else {
+			return new Rating(Rating.State.UNCLEAR);
+		}
+	}
+
+	@Override
+	public Value getValue(Question question) {
+		Fact fact = getValueFact(question);
+		if (fact == null) {
+			return UndefinedValue.getInstance();
+		}
+		else {
+			Value value = fact.getValue();
+			if (question instanceof QuestionOC && value instanceof NumValue) {
+				QuestionOC qoc = (QuestionOC) question;
+				Num2ChoiceSchema schema = qoc.getSchemaForQuestion();
+				NumValue numValue = (NumValue) value;
+				if (schema != null) {
+					return schema.getValueForNum((Double) numValue.getValue(),
+							qoc.getAllAlternatives(), session);
+				}
+			}
+			return value;
+		}
+	}
+
+	@Override
+	public Value getValue(TerminologyObject object, PSMethod psmethod) {
+		return valueStorage.getAggregator(object).getMergedFact(psmethod).getValue();
+	}
+
+	@Override
+	public Indication getIndication(InterviewObject interviewElement) {
+		Fact fact = getInterviewFact(interviewElement);
+		if (fact == null) {
+			return Indication.getDefaultIndication();
+		}
+		else {
+			return (Indication) getInterviewFact(interviewElement).getValue();
+		}
+	}
+
+	@Override
+	public List<Question> getAnsweredQuestions() {
+		List<Question> questions = new LinkedList<Question>();
+		for (Question q : session.getKnowledgeBase().getQuestions()) {
+			Fact mergedFact = valueStorage.getAggregator(q).getMergedFact();
+			if (mergedFact != null && UndefinedValue.isNotUndefinedValue(mergedFact.getValue())) {
+				questions.add(q);
+			}
+		}
+		return questions;
+	}
+
+	@Override
+	public List<Solution> getSolutions(Rating.State state) {
+		List<Solution> result = new LinkedList<Solution>();
+		for (Solution diag : getSession().getKnowledgeBase().getSolutions()) {
+			if (getState(diag).getState().equals(state)) {
+				result.add(diag);
+			}
+		}
+		return result;
+	}
+}
