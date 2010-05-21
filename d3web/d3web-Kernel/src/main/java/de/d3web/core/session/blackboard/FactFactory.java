8ddbe6dbb225624d304cff76ea6992aaaf84dcce
Markus Friedrich <markus.friedrich@denkbares.com>
2010-05-21 15:12:39 +0000

    Added new method FactFactory.createUserEnteredFact(KnowledgeBase kb, String questionID, String answerID)

--- d3web/d3web-Kernel/src/main/java/de/d3web/core/session/blackboard/FactFactory.java
+++ d3web/d3web-Kernel/src/main/java/de/d3web/core/session/blackboard/FactFactory.java
@@ -1,24 +1,34 @@
 package de.d3web.core.session.blackboard;
 
 import de.d3web.core.inference.PSMethod;
+import de.d3web.core.knowledge.KnowledgeBase;
 import de.d3web.core.knowledge.TerminologyObject;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionChoice;
+import de.d3web.core.manage.KnowledgeBaseManagement;
 import de.d3web.core.session.Value;
+import de.d3web.core.session.values.Choice;
+import de.d3web.core.session.values.ChoiceValue;
 import de.d3web.indication.inference.PSMethodUserSelected;
 
 /**
  * A factory to create {@link Fact} instances.
+ * 
  * @author joba
  */
 public class FactFactory {
 
 	/**
-	 * Creates a new fact assigning the specified {@link Value} to the specified 
-	 * {@link TerminologyObject}. The specified source is responsible for the setting
-	 * the value, which acts in the context of the specified {@link PSMethod}. 
+	 * Creates a new fact assigning the specified {@link Value} to the specified
+	 * {@link TerminologyObject}. The specified source is responsible for the
+	 * setting the value, which acts in the context of the specified
+	 * {@link PSMethod}.
+	 * 
 	 * @param terminologyObject the specified {@link TerminologyObject} instance
 	 * @param value the specified {@link Value} instance
 	 * @param source the responsible source
-	 * @param psMethod the fact is created in the context of the specified {@link PSMethod}
+	 * @param psMethod the fact is created in the context of the specified
+	 *        {@link PSMethod}
 	 * @return a newly created {@link Fact} instance
 	 */
 	public static Fact createFact(TerminologyObject terminologyObject,
@@ -27,9 +37,10 @@ public class FactFactory {
 	}
 
 	/**
-	 * A new fact is created assigning the specified {@link Value} to the specified 
-	 * {@link TerminologyObject}. The source and psMethod context of this fact is the
-	 * user (i.e., {@link PSMethodUserSelected}).
+	 * A new fact is created assigning the specified {@link Value} to the
+	 * specified {@link TerminologyObject}. The source and psMethod context of
+	 * this fact is the user (i.e., {@link PSMethodUserSelected}).
+	 * 
 	 * @param terminologyObject the specified {@link TerminologyObject} instance
 	 * @param value the specified {@link Value} instance
 	 * @return a newly created {@link Fact} instance
@@ -38,6 +49,32 @@ public class FactFactory {
 			TerminologyObject terminologyObject, Value value) {
 		return new DefaultFact(terminologyObject, value, PSMethodUserSelected
 				.getInstance(), PSMethodUserSelected.getInstance());
+	}
+
+	/**
+	 * A new fact is created assigning a {@link ChoiceValue} to a
+	 * {@link QuestionChoice}. The {@link QuestionChoice} and the {@link Choice}
+	 * are searched by their ids in the given {@link KnowledgeBase}. The source
+	 * and psMethod context of this fact is the user (i.e.,
+	 * {@link PSMethodUserSelected}).
+	 * 
+	 * @param kb {@link KnowledgeBase}
+	 * @param questionID ID of the {@link QuestionChoice}
+	 * @param answerID ID of the {@link Choice}
+	 * @return a newly created {@link Fact} instance or null, if the
+	 *         {@link QuestionChoice} or {@link Choice} could not be found
+	 */
+	public static Fact createUserEnteredFact(KnowledgeBase kb, String questionID, String answerID) {
+		Question question = kb.searchQuestion(questionID);
+		if (question != null && question instanceof QuestionChoice) {
+			QuestionChoice qc = (QuestionChoice) question;
+			KnowledgeBaseManagement kbm = KnowledgeBaseManagement.createInstance(kb);
+			Choice choice = kbm.findChoice(qc, answerID);
+			if (choice != null) {
+				return createUserEnteredFact(qc, new ChoiceValue(choice));
+			}
+		}
+		return null;
 	}
 
 }
