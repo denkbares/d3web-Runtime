5bb83a5d82c90eedfe415b0ee97c362200dbc792
Markus Friedrich <markus.friedrich@denkbares.com>
2010-05-28 10:48:47 +0000

    * Stabilized order of Plugins in plugin.xml (now sorted by id) * Added new KB to Global Tests

--- d3web/d3web-Persistence-Basic/src/main/java/de/d3web/plugin/io/PluginConfigPersistenceHandler.java
+++ d3web/d3web-Persistence-Basic/src/main/java/de/d3web/plugin/io/PluginConfigPersistenceHandler.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2009 denkbares GmbH
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package de.d3web.plugin.io;
 
@@ -22,6 +22,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.w3c.dom.Document;
@@ -37,10 +40,11 @@ import de.d3web.core.io.progress.ProgressListener;
 import de.d3web.core.io.utilities.Util;
 import de.d3web.core.io.utilities.XMLUtil;
 import de.d3web.core.knowledge.KnowledgeBase;
-import de.d3web.plugin.PluginConfig;
 import de.d3web.plugin.Plugin;
+import de.d3web.plugin.PluginConfig;
 import de.d3web.plugin.PluginEntry;
 import de.d3web.plugin.PluginManager;
+
 /**
  * A KnowledgeReader/Writer for the configuration of the extensions
  * 
@@ -95,37 +99,43 @@ public class PluginConfigPersistenceHandler implements KnowledgeReader,
 		root.appendChild(plugins);
 		listener.updateProgress(0, "Saving plugin configuration");
 		float count = 1;
-		Collection<PluginEntry> entries = getEntries(kb);
+		List<PluginEntry> entries = new LinkedList<PluginEntry>(getEntries(kb));
+		Collections.sort(entries, new Comparator<PluginEntry>() {
+
+			@Override
+			public int compare(PluginEntry o1, PluginEntry o2) {
+				return o1.getPlugin().getPluginID().compareTo(o2.getPlugin().getPluginID());
+			}
+		});
 		if (entries != null) {
 			int max = entries.size();
-			for (PluginEntry conf: entries) {
+			for (PluginEntry conf : entries) {
 				Element pluginElement = doc.createElement("plugin");
 				Plugin plugin = conf.getPlugin();
 				pluginElement.setAttribute("ID", plugin.getPluginID());
-				pluginElement.setAttribute("necessary", ""+conf.isNecessary());
-				pluginElement.setAttribute("autodetect", ""+conf.isAutodetect());
+				pluginElement.setAttribute("necessary", "" + conf.isNecessary());
+				pluginElement.setAttribute("autodetect", "" + conf.isAutodetect());
 				plugins.appendChild(pluginElement);
-				listener.updateProgress(count++/max, "Saving plugin configuration");
+				listener.updateProgress(count++ / max, "Saving plugin configuration");
 			}
 		}
 		Element psmethods = doc.createElement("psmethods");
 		root.appendChild(psmethods);
-		for (PSConfig ps: kb.getPsConfigs()) {
+		for (PSConfig ps : kb.getPsConfigs()) {
 			psmethods.appendChild(PersistenceManager.getInstance().writeFragment(ps, doc));
 		}
 		Util.writeDocumentToOutputStream(doc, stream);
 	}
-
 
 	@Override
 	public int getEstimatedSize(KnowledgeBase kb) {
 		Collection<PluginEntry> entries = getEntries(kb);
 		return (entries != null) ? entries.size() : 0;
 	}
-	
+
 	private Collection<PluginEntry> getEntries(KnowledgeBase kb) {
 		Collection<KnowledgeSlice> pluginconfigs = kb.getAllKnowledgeSlicesFor(PSMethod.class);
-		for (KnowledgeSlice ks: pluginconfigs) {
+		for (KnowledgeSlice ks : pluginconfigs) {
 			if (ks instanceof PluginConfig) {
 				PluginConfig pc = (PluginConfig) ks;
 				return pc.getEntries();
