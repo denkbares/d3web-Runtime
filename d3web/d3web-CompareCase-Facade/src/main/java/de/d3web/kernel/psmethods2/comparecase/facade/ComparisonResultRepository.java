e0e1fa7d4d26818fcfb18efab87b7e56572762a1
Markus Friedrich <markus.friedrich@denkbares.com>
2010-07-19 13:47:25 +0000

    Intermediate commit for renamings

--- d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psmethods2/comparecase/facade/ComparisonResultRepository.java
+++ d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psmethods2/comparecase/facade/ComparisonResultRepository.java
@@ -18,7 +18,7 @@
  * site: http://www.fsf.org.
  */
 
-package de.d3web.kernel.psMethods.comparecase2.facade;
+package de.d3web.kernel.psmethods2.comparecase.facade;
 
 import java.util.Collection;
 import java.util.Collections;
--- d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psmethods2/comparecase/facade/SimpleResult.java
+++ d3web/d3web-CompareCase-Facade/src/main/java/de/d3web/kernel/psmethods2/comparecase/facade/SimpleResult.java
@@ -18,7 +18,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package de.d3web.kernel.psMethods.comparecase2.facade;
+package de.d3web.kernel.psmethods2.comparecase.facade;
 import java.util.Collection;
 
 import de.d3web.caserepository.CaseObject;
