69fa78d6309854dd5b10f1e894bf99cd1b24b3cb
Sebastian Furth <sebastian.furth@denkbares.com>
2011-02-22 09:47:07 +0000

    * necessary relations are not necessary any more to set the state of a XCL-List to SUGGESTED

--- d3web/d3web-Plugin-XCL/src/main/java/de/d3web/xcl/DefaultScoreAlgorithm.java
+++ d3web/d3web-Plugin-XCL/src/main/java/de/d3web/xcl/DefaultScoreAlgorithm.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2010 denkbares GmbH, Würzburg, Germany
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -105,7 +105,7 @@ public class DefaultScoreAlgorithm implements ScoreAlgorithm {
 						? new Rating(Rating.State.ESTABLISHED)
 						: new Rating(Rating.State.SUGGESTED);
 			}
-			if (score >= getSuggestedThreshold(model) && hasAllNecessary) {
+			if (score >= getSuggestedThreshold(model)) {
 				return new Rating(Rating.State.SUGGESTED);
 			}
 		}
