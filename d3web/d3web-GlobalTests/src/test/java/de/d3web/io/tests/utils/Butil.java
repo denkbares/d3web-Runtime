f081bf0de21e317a2bd9b63dfcc73aa1d25bde71
Markus Friedrich <markus.friedrich@denkbares.com>
2011-08-03 16:20:35 +0000

    Removed unused methods

--- d3web/d3web-GlobalTests/src/test/java/de/d3web/io/tests/utils/Butil.java
+++ d3web/d3web-GlobalTests/src/test/java/de/d3web/io/tests/utils/Butil.java
@@ -24,75 +24,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.LinkedList;
 
 public class Butil {
-
-	public static String readBytes(Reader r) {
-		int zeichen = 0;
-		LinkedList<Integer> ints = new LinkedList<Integer>();
-		while (true) {
-
-			try {
-				zeichen = r.read();
-			}
-			catch (IOException e) {
-				break;
-			}
-			catch (OutOfMemoryError e1) {
-				break;
-			}
-
-			if (zeichen == -1) {
-				break;
-			}
-
-			ints.add(zeichen);
-		}
-
-		StringBuilder buffi = new StringBuilder();
-		for (Integer i : ints) {
-
-			if ((i.intValue() == 128) || (i.intValue() == 228)
-					|| (i.intValue() == 252) || (i.intValue() == 246)
-					|| (i.intValue() == 214) || (i.intValue() == 196)
-					|| (i.intValue() == 220) || (i.intValue() == 223)) {
-				if (i.intValue() == 128) {
-					buffi.append('€');
-				}
-				if (i.intValue() == 228) {
-					buffi.append('ä');
-				}
-				if (i.intValue() == 252) {
-					buffi.append('ü');
-				}
-				if (i.intValue() == 246) {
-					buffi.append('ö');
-				}
-				if (i.intValue() == 214) {
-					buffi.append('ü');
-				}
-				if (i.intValue() == 196) {
-					buffi.append('Ö');
-				}
-				if (i.intValue() == 220) {
-					buffi.append('Ü');
-				}
-				if (i.intValue() == 223) {
-					buffi.append('ß');
-				}
-			}
-			else {
-				buffi.append(((char) i.intValue()));
-			}
-		}
-		return buffi.toString();
-	}
-
-	public static String ReaderToString(Reader r) {
-		return readBytes(r).replace('@', '%');
-	}
 
 	/**
 	 * Reads from a Reader with UTF8 encoding.
