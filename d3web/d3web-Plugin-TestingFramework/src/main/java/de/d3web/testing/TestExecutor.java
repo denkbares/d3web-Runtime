16723e60d191c54289a7a310e25219f5a131c5dd
Jochen Reutelshöfer <jochen.reutelshoefer@denkbares.com>
2012-07-04 12:33:52 +0000

    * prepared TestExecutor to use CombinedProgressListener * some refactoring due to this * TestingFramework depends on d3web-Utils * CombinedProgressListener: now totalSize can be set after creation time (second constructor)

--- d3web/d3web-Plugin-TestingFramework/src/main/java/de/d3web/testing/TestExecutor.java
+++ d3web/d3web-Plugin-TestingFramework/src/main/java/de/d3web/testing/TestExecutor.java
@@ -2,7 +2,10 @@ package de.d3web.testing;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -10,6 +13,9 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import de.d3web.core.io.progress.CombinedProgressListener;
+import de.d3web.testing.Message.Type;
 
 /*
  * Copyright (C) 2012 denkbares GmbH
@@ -41,13 +47,15 @@ public class TestExecutor {
 
 	private final Collection<TestObjectProvider> testObjectProviders;
 	private final List<ExecutableTest> tests;
+	private final CombinedProgressListener progressListener;
 
 	/**
 	 * Creates a TestExecutor with the given task list and TestObjectProvider.
 	 */
-	public TestExecutor(Collection<TestObjectProvider> providers, List<ExecutableTest> testAndItsParameters) {
+	public TestExecutor(Collection<TestObjectProvider> providers, List<ExecutableTest> testAndItsParameters, CombinedProgressListener listener) {
 		this.testObjectProviders = providers;
 		this.tests = testAndItsParameters;
+		this.progressListener = listener;
 	}
 
 	private static <T> T cast(Object testObject, Class<T> testObjectClass) {
@@ -76,17 +84,53 @@ public class TestExecutor {
 		long buildStartTime = System.currentTimeMillis();
 		BuildResult build = new BuildResult(buildNumber);
 
+		Map<ExecutableTest, Map<TestObjectProvider, List<?>>> allTestsAndTestobjects = new HashMap<ExecutableTest, Map<TestObjectProvider, List<?>>>();
+
+		int testObjectsTotal = 0;
 		for (ExecutableTest testAndItsParameters : tests) {
 
 			String[] array = testAndItsParameters.getArguments();
-			TestResult testResult = this.runTest(testAndItsParameters.getTest(), array[0],
-					Arrays.copyOfRange(array, 1,
-							array.length));
+
+			String[] testArgs = Arrays.copyOfRange(array, 1,
+					array.length);
+			String testObjectID = array[0];
+			TestResult checkArgs = checkArgs(testAndItsParameters.getTest(), testObjectID,
+					testArgs);
+
+			if (checkArgs != null && checkArgs.getType().equals(Type.ERROR)) {
+				build.addTestResult(checkArgs);
+			}
+			else {
+				Map<TestObjectProvider, List<?>> testObjectsForTest = this.collectTestObjects(
+						testAndItsParameters.getTest(), testObjectID,
+						testArgs
+						);
+
+				allTestsAndTestobjects.put(testAndItsParameters, testObjectsForTest);
+				testObjectsTotal += countTestObjects(testObjectsForTest);
+			}
+		}
+
+		// now the total count of tests to be executed is clear and can be told
+		// to the progress listener
+		progressListener.setTotalSize(testObjectsTotal);
+
+		for (ExecutableTest test : allTestsAndTestobjects.keySet()) {
+			// next major step for combined progress listener
+			progressListener.next(countTestObjects(allTestsAndTestobjects.get(test)));
+
+			String[] array = test.getArguments();
+			String[] testArgs = Arrays.copyOfRange(array, 1,
+					array.length);
+			TestResult testResult = executeTests(test.getTest(), testArgs,
+					allTestsAndTestobjects.get(test));
+
 			if (testResult == null) {
 				testResult = new TestResult(
-						testAndItsParameters.getTest().getClass().getSimpleName(),
-						testAndItsParameters.getArguments());
-				testResult.addMessage(array[0], new Message(Message.Type.ERROR,
+						test.getTest().getClass().getSimpleName(),
+						test.getArguments());
+				testResult.addMessage(test.getArguments()[0], new Message(
+						Message.Type.ERROR,
 						"Unexpected error while executing test."));
 			}
 			build.addTestResult(testResult);
@@ -107,9 +151,25 @@ public class TestExecutor {
 	 * @param args The parameters for the test execution
 	 * @return
 	 */
-	public <T> TestResult runTest(final Test<T> t, final String testObjectID, final String[] args) {
+	public <T> Map<TestObjectProvider, List<?>> collectTestObjects(final Test<T> t, final String testObjectID, final String[] args) {
 
-		final String testName = t.getClass().getSimpleName();
+		final Map<TestObjectProvider, List<?>> allTestObjects = new HashMap<TestObjectProvider, List<?>>();
+
+		// retrieve all test objects
+		for (TestObjectProvider testObjectProvider : testObjectProviders) {
+
+			List<? extends Object> testObjects = testObjectProvider.getTestObjects(
+					t.getTestObjectClass(),
+					testObjectID);
+			allTestObjects.put(testObjectProvider, testObjects);
+
+		}
+
+		return allTestObjects;
+	}
+
+	private <T> TestResult checkArgs(final Test<T> t, final String testObjectID, final String[] args) {
+
 		ArgsCheckResult argsCheckResult = t.checkArgs(args);
 		if (argsCheckResult.hasError()) {
 			TestResult result = new TestResult(t.getClass().getSimpleName(),
@@ -129,7 +189,21 @@ public class TestExecutor {
 
 			return result;
 		}
+		return null;
+	}
 
+	private static int countTestObjects(Map<TestObjectProvider, List<?>> allTestObjects) {
+		int count = 0;
+		Set<TestObjectProvider> keySet = allTestObjects.keySet();
+		for (TestObjectProvider p : keySet) {
+			List<?> list = allTestObjects.get(p);
+			count += list.size();
+		}
+		return count;
+	}
+
+	private <T> TestResult executeTests(final Test<T> t, final String[] args, final Map<TestObjectProvider, List<?>> allTestObjects) {
+		final String testName = t.getClass().getSimpleName();
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 
 		Callable<TestResult> c = new Callable<TestResult>() {
@@ -138,14 +212,13 @@ public class TestExecutor {
 			public TestResult call() throws Exception {
 				TestResult result = new TestResult(testName, args);
 
-				int totalNumberOfTestObjects = 0;
+				int totalCountOfTestobjects = countTestObjects(allTestObjects);
+
+				// finally run the tests
+				int currentIndex = 0;
 				for (TestObjectProvider testObjectProvider : testObjectProviders) {
+					for (Object testObject : allTestObjects.get(testObjectProvider)) {
 
-					List<?> testObjects = testObjectProvider.getTestObjects(t.getTestObjectClass(),
-							testObjectID);
-					totalNumberOfTestObjects += testObjects.size();
-
-					for (Object testObject : testObjects) {
 						if (testObject != null) {
 							Message message = t.execute(cast(testObject, t.getTestObjectClass()),
 									args);
@@ -156,10 +229,18 @@ public class TestExecutor {
 							result.addMessage(testName, new Message(Message.Type.ERROR,
 									"Test-object was null. (Check TestObjectProviders)"));
 						}
+						currentIndex++;
+						// update progress listener
+						progressListener.updateProgress(
+								((float) (currentIndex)) / totalCountOfTestobjects,
+								t.getClass().getSimpleName() + ": "
+										+ testObjectProvider.getTestObjectName(testObject));
 
 					}
+
 				}
-				if (totalNumberOfTestObjects == 0) {
+
+				if (totalCountOfTestobjects == 0) {
 					result.addMessage(testName, new Message(Message.Type.ERROR,
 							"No test-object found. (Check TestObjectProviders)"));
 
