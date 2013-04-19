8fe8c7826f1fbcfb6233ffdb53c79e135b38eb94
Markus Friedrich <markus.friedrich@denkbares.com>
2013-04-19 17:30:04 +0000

    Added a wrapper FormStrategy to convert another FormStrategy to a SingeQuestionFormStrategy

---
+++ d3web/d3web-Plugin-Interview/src/main/java/de/d3web/interview/SingleQuestionFormStrategyWrapper.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2013 denkbares GmbH
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package de.d3web.interview;
+
+import java.util.List;
+
+import de.d3web.core.knowledge.InterviewObject;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.session.Session;
+import de.d3web.core.session.values.UndefinedValue;
+
+/**
+ * Wraps a {@link FormStrategy} and returns always a{@link DefaultForm}
+ * containing one question or the {@link EmptyForm}
+ * 
+ * @author Markus Friedrich (denkbares GmbH)
+ * @created 19.04.2013
+ */
+public class SingleQuestionFormStrategyWrapper implements FormStrategy {
+
+	private final FormStrategy wrappedStrategy;
+
+	public SingleQuestionFormStrategyWrapper(FormStrategy wrappedStrategy) {
+		super();
+		this.wrappedStrategy = wrappedStrategy;
+	}
+
+	@Override
+	public Form nextForm(List<InterviewObject> agendaEntries, Session session) {
+		Form nextForm = wrappedStrategy.nextForm(agendaEntries, session);
+		return getNextUnansweredQuestionAsForm(nextForm, session);
+	}
+
+	@Override
+	public Form getForm(InterviewObject object, Session session) {
+		Form nextForm = wrappedStrategy.getForm(object, session);
+		return getNextUnansweredQuestionAsForm(nextForm, session);
+	}
+
+	private Form getNextUnansweredQuestionAsForm(Form form, Session session) {
+		for (Question q : form.getActiveQuestions()) {
+			if (UndefinedValue.isUndefinedValue(session.getBlackboard().getValue(q))) {
+				return new DefaultForm(q.getName(), q, session);
+			}
+		}
+		return EmptyForm.getInstance();
+	}
+
+}
