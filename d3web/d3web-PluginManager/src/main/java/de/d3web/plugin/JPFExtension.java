2fb1089972314316473c3fd8f37574c63ded29d1
Volker Belli <volker.belli@denkbares.com>
2014-02-14 07:48:59 +0000

    Added capability to specify an optional constructor call in the class attribute of a plugin. When construction a new instance or singleton, not the default constructor is used, but the one having the specified arguments. Currently the argument types String, Integer and Double are supported. For String you can use single (') or double (") quotes. individual arguments are also java-likely  separated by ","
    example:
    <parameter id="class" value="my.path.to.clazz.ClassName" />
    <parameter id="class" value="my.path.to.clazz.ClassName('string-parameter')" />
    <parameter id="class" value="my.path.to.clazz.ClassName(50)" />
    <parameter id="class" value="my.path.to.clazz.ClassName(50, 'string-parameter')" />

--- d3web/d3web-PluginManager/src/main/java/de/d3web/plugin/JPFExtension.java
+++ d3web/d3web-PluginManager/src/main/java/de/d3web/plugin/JPFExtension.java
@@ -18,6 +18,7 @@
  */
 package de.d3web.plugin;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -27,6 +28,7 @@ import org.java.plugin.registry.Extension;
 import org.java.plugin.registry.Extension.Parameter;
 import org.java.plugin.registry.PluginDescriptor;
 
+import de.d3web.strings.Strings;
 import de.d3web.utils.Log;
 
 /**
@@ -58,8 +60,56 @@ public class JPFExtension implements de.d3web.plugin.Extension {
 	}
 
 	public Object getNewInstance(String className) {
-		PluginDescriptor declaringPluginDescriptor = extension
-				.getDeclaringPluginDescriptor();
+		PluginDescriptor declaringPluginDescriptor =
+				extension.getDeclaringPluginDescriptor();
+
+		final String constructor = className;
+
+		// allow arguments to class as constructor parameters
+		Object[] argValues = new Object[0];
+		Class<?>[] argTypes = new Class[0];
+		int argIndex = constructor.indexOf('(');
+		if (argIndex > 0) {
+			// parse constructor call
+			className = constructor.substring(0, argIndex).trim();
+			String argsString = constructor.substring(argIndex + 1, constructor.lastIndexOf(')'));
+			String[] arguments = argsString.trim().split("\\s*,\\s*");
+
+			// init arguments and argument types
+			argValues = new Object[arguments.length];
+			argTypes = new Class[arguments.length];
+			for (int i = 0; i < arguments.length; i++) {
+				String argument = arguments[i];
+				// accept string types
+				if (argument.matches("'.*'|\".*\"")) {
+					argTypes[i] = String.class;
+					argValues[i] = Strings.unquote(argument.substring(1, argument.length() - 1),
+							argument.charAt(0));
+				}
+				// accept integer arguments
+				else if (argument.matches("\\d+")) {
+					argTypes[i] = Integer.class;
+					argValues[i] = new Integer(argument);
+				}
+				// accept double arguments
+				else if (argument.matches("\\d+\\.\\d+")) {
+					argTypes[i] = Integer.class;
+					argValues[i] = new Integer(argument);
+				}
+				// allow other types as required
+				// if not, an exception if thrown
+				else {
+					String message = "Error in plugin '" + getPluginID() +
+							"', extension '" + getID() + "'. " +
+							"The constructor '" + constructor +
+							"' specified in the plugin has an unsupported argument type.  " +
+							"This is a strong evidence for an incorrect plugin.";
+					Log.severe(message);
+					throw new IllegalArgumentException(message);
+				}
+			}
+		}
+
 		try {
 			// secures that the plugin containing the definition of the
 			// extension
@@ -72,6 +122,11 @@ public class JPFExtension implements de.d3web.plugin.Extension {
 		ClassLoader classLoader = manager.getPluginClassLoader(declaringPluginDescriptor);
 		try {
 			Class<?> clazz = classLoader.loadClass(className);
+			// instantiate with parameters
+			if (argValues.length > 0) {
+				return clazz.getConstructor(argTypes).newInstance(argValues);
+			}
+			// instantiate normally
 			return clazz.newInstance();
 		}
 		catch (ClassNotFoundException e) {
@@ -84,6 +139,22 @@ public class JPFExtension implements de.d3web.plugin.Extension {
 		catch (InstantiationException e) {
 			Log.severe("Error in plugin '" + getPluginID() + "', extension '" + getID() + "'. " +
 					"It was not possible to instantiate an object. " +
+					"This is a strong evidence for an incorrect plugin.",
+					e);
+			throw new InstantiationError(e.getMessage());
+		}
+		catch (NoSuchMethodException e) {
+			Log.severe("Error in plugin '" + getPluginID() + "', extension '" + getID() + "'. " +
+					"It was not possible to instantiate an object with constructor '" +
+					constructor + "'. " +
+					"This is a strong evidence for an incorrect plugin.",
+					e);
+			throw new NoSuchMethodError(e.getMessage());
+		}
+		catch (InvocationTargetException e) {
+			Log.severe("Error in plugin '" + getPluginID() + "', extension '" + getID() + "'. " +
+					"It was not possible to instantiate an object with constructor '" +
+					constructor + "'. " +
 					"This is a strong evidence for an incorrect plugin.",
 					e);
 			throw new InstantiationError(e.getMessage());
