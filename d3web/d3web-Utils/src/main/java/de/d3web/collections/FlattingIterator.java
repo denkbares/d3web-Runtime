b9cb9df77b1f085d11270f1bd94bc056b3bddb05
Markus Friedrich <markus.friedrich@denkbares.com>
2014-03-21 15:40:41 +0000

    Replaced java 7 methods

--- d3web/d3web-Utils/src/main/java/de/d3web/collections/FlattingIterator.java
+++ d3web/d3web-Utils/src/main/java/de/d3web/collections/FlattingIterator.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013 denkbares GmbH
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -22,25 +22,27 @@ import java.util.Collections;
 import java.util.Iterator;
 
 /**
- * Iterates over the elements of an iterable of iterables, e.g. you can iterate over the Sting items
- * of a Collection&lt;Set&lt;String&gt;&gt;.
- *
+ * Iterates over the elements of an iterable of iterables, e.g. you can iterate
+ * over the Sting items of a Collection&lt;Set&lt;String&gt;&gt;.
+ * 
  * @author Volker Belli (denkbares GmbH)
  * @created 14.01.2013
  */
 public class FlattingIterator<T> implements Iterator<T> {
 
 	public static interface IteratorFactory<S, T> {
+
 		Iterator<T> create(S sourceObject);
 	}
 
 	private final Iterator<?> iterables;
 	private final IteratorFactory<Object, T> factory;
 
-	private Iterator<T> current = Collections.emptyIterator();
+	private Iterator<T> current = Collections.<T> emptyList().iterator();
 
 	public <I extends Iterable<T>> FlattingIterator(Iterator<I> iterables) {
 		this(iterables, new IteratorFactory<I, T>() {
+
 			@Override
 			public Iterator<T> create(I sourceObject) {
 				return sourceObject.iterator();
@@ -62,13 +64,12 @@ public class FlattingIterator<T> implements Iterator<T> {
 		this.factory = (IteratorFactory<Object, T>) factory;
 	}
 
-	@SuppressWarnings("unchecked")
 	private void proceed() {
 		// proceed if current has ended and we have further ones
 		while (!current.hasNext() && iterables.hasNext()) {
 			this.current = iterables.hasNext()
 					? factory.create(iterables.next())
-					: Collections.<T>emptyIterator();
+					: Collections.<T> emptyList().iterator();
 		}
 	}
 
--- d3web/d3web-Utils/src/main/java/de/d3web/collections/GeneralizedSuffixTree.java
+++ d3web/d3web-Utils/src/main/java/de/d3web/collections/GeneralizedSuffixTree.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013 denkbares GmbH
- *
+ * 
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- *
+ * 
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -27,39 +27,42 @@ import java.util.NavigableMap;
 import java.util.Set;
 import java.util.TreeMap;
 
+import de.d3web.collections.FlattingIterator.IteratorFactory;
 import de.d3web.strings.Strings;
 
-import static de.d3web.collections.FlattingIterator.IteratorFactory;
-
 /**
- * Implementation of generalized suffix tree that is capable to take any element types to be stored
- * for any strings (String --> E). It is implemented like an MultiMap and allows to store multiple
- * objects for the same strings. It also allows to receive the whole set of elements added that
- * matches a specified substring.
+ * Implementation of generalized suffix tree that is capable to take any element
+ * types to be stored for any strings (String --> E). It is implemented like an
+ * MultiMap and allows to store multiple objects for the same strings. It also
+ * allows to receive the whole set of elements added that matches a specified
+ * substring.
  * <p/>
- * In contrast to existing generalized suffix trees, our implementation allows to easily remove
- * key-value-pairs from the tree, keeping track if the specified combination has been inserted or
- * not.
+ * In contrast to existing generalized suffix trees, our implementation allows
+ * to easily remove key-value-pairs from the tree, keeping track if the
+ * specified combination has been inserted or not.
  * <p/>
- * The implementation is based of a DefaultMultiMap mapping each key to the values. Additionally for
- * each key there is a tree-map of all suffixes to the keys.
- *
+ * The implementation is based of a DefaultMultiMap mapping each key to the
+ * values. Additionally for each key there is a tree-map of all suffixes to the
+ * keys.
+ * 
  * @author Volker Belli (denkbares GmbH)
  * @created 13.03.14.
  */
 public class GeneralizedSuffixTree<E> extends DefaultMultiMap<String, E> {
 
 	/**
-	 * Here we store for each suffix all the key strings that have been used to generate the suffix.
-	 * This map uses a tree-map to easily allow to access for any suffix all suffixes that are
-	 * stored, because they are a sub-map of the tree-map. Using this, searching an infix is looking
-	 * for the node in the suffix-tree, getting a sub-map of all suffixes starting with the infix
-	 * and for them get the values that have been added.
+	 * Here we store for each suffix all the key strings that have been used to
+	 * generate the suffix. This map uses a tree-map to easily allow to access
+	 * for any suffix all suffixes that are stored, because they are a sub-map
+	 * of the tree-map. Using this, searching an infix is looking for the node
+	 * in the suffix-tree, getting a sub-map of all suffixes starting with the
+	 * infix and for them get the values that have been added.
 	 * <p/>
-	 * We use the tree-set also for the values, so we get a natural ordering of the keys.
+	 * We use the tree-set also for the values, so we get a natural ordering of
+	 * the keys.
 	 */
 	private final DefaultMultiMap<String, String> suffixTree = new DefaultMultiMap<String, String>(
-			MultiMaps.<String>treeFactory(), MultiMaps.<String>treeFactory());
+			MultiMaps.<String> treeFactory(), MultiMaps.<String> treeFactory());
 
 	@Override
 	public boolean put(String key, E value) {
@@ -114,15 +117,18 @@ public class GeneralizedSuffixTree<E> extends DefaultMultiMap<String, E> {
 		// and all other ones to filter the found matches
 		final Iterable<String> keys = findKeys(infixes[0]);
 		return new Iterable<E>() {
+
 			@Override
 			public Iterator<E> iterator() {
 				return new FlattingIterator<E>(keys, new IteratorFactory<String, E>() {
+
 					@Override
 					public Iterator<E> create(String key) {
-						// filter all keys that are not matching all other infixes
+						// filter all keys that are not matching all other
+						// infixes
 						for (int i = 1; i < infixes.length; i++) {
 							if (!Strings.containsIgnoreCase(key, infixes[i])) {
-								return Collections.emptyIterator();
+								return Collections.<E> emptyList().iterator();
 							}
 						}
 						// filter duplicate items from the values
@@ -138,6 +144,7 @@ public class GeneralizedSuffixTree<E> extends DefaultMultiMap<String, E> {
 		if (range == null) return Collections.emptyList();
 		final Collection<Set<String>> keysOfSuffixes = range.values();
 		return new Iterable<String>() {
+
 			@Override
 			public Iterator<String> iterator() {
 				return new FlattingIterator<String>(keysOfSuffixes);
@@ -152,7 +159,8 @@ public class GeneralizedSuffixTree<E> extends DefaultMultiMap<String, E> {
 		String start = tree.ceilingKey(infix);
 		if (start == null) return null;
 		// get the last suffix that starts with infix
-		// (we accept that the following will only work well if \uffff is not used)
+		// (we accept that the following will only work well if \uffff is not
+		// used)
 		String end = tree.floorKey(infix + '\uffff');
 		if (end == null) return null;
 		return tree.subMap(start, true, end, true);
@@ -173,9 +181,9 @@ public class GeneralizedSuffixTree<E> extends DefaultMultiMap<String, E> {
 	}
 
 	/**
-	 * Creates all suffixes for a given phrase. The phrase is splitted by whitespaces and each
-	 * resulting token is indexed separately.
-	 *
+	 * Creates all suffixes for a given phrase. The phrase is splitted by
+	 * whitespaces and each resulting token is indexed separately.
+	 * 
 	 * @param phrase the phrase to get the suffixes for
 	 * @return all suffixes of all contained words
 	 */
