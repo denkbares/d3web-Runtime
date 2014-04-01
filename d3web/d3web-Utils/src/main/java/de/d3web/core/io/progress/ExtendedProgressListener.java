f7aad981e8a4c41d8267158bade98d13c4c992c7
Albrecht Striffler <albrecht.striffler@denkbares.com>
2014-04-01 07:55:46 +0000

    Extended Interface for ProgressListener.

---
+++ d3web/d3web-Utils/src/main/java/de/d3web/core/io/progress/ExtendedProgressListener.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 denkbares GmbH, Germany
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+
+package de.d3web.core.io.progress;
+
+/**
+ * A ProgressListener with a slightly extended interface.
+ * <p/>
+ * Created by Albrecht Striffler (denkbares GmbH) on 31.03.14.
+ */
+public interface ExtendedProgressListener extends ProgressListener {
+
+	/**
+	 * Returns the current progress of this listener.
+	 */
+	float getProgress();
+}
