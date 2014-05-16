536079de2c5d5d1e147876fd69308bb53cdf369b
Volker Belli <volker.belli@denkbares.com>
2014-05-16 13:46:41 +0000

    added methods to check extensions of files

--- d3web/d3web-Utils/src/main/java/de/d3web/utils/Files.java
+++ d3web/d3web-Utils/src/main/java/de/d3web/utils/Files.java
@@ -19,9 +19,9 @@ public class Files {
 	private static final int TEMP_DIR_ATTEMPTS = 1000;
 
 	/**
-	 * Create a new temporary directory. Use {@link #recursiveDelete(File)} to
-	 * clean this directory up since it isn't deleted automatically
-	 * 
+	 * Create a new temporary directory. Use {@link #recursiveDelete(File)} to clean this directory
+	 * up since it isn't deleted automatically
+	 *
 	 * @return the new directory
 	 * @throws IOException if there is an error creating the temporary directory
 	 */
@@ -45,7 +45,7 @@ public class Files {
 
 	/**
 	 * Recursively delete file or directory
-	 * 
+	 *
 	 * @param fileOrDir the file or dir to delete
 	 * @return true iff all files are successfully deleted
 	 */
@@ -64,11 +64,11 @@ public class Files {
 
 	/**
 	 * Returns the contents of the specified file as a byte[].
-	 * 
-	 * @created 01.10.2013
+	 *
 	 * @param file the input file to read from
 	 * @return the content of the file
 	 * @throws IOException if the specified file cannot be read completely
+	 * @created 01.10.2013
 	 */
 	public static byte[] getBytes(File file) throws IOException {
 		return Streams.getBytesAndClose(new FileInputStream(file));
@@ -76,11 +76,11 @@ public class Files {
 
 	/**
 	 * Returns the contents of the specified file as a String.
-	 * 
-	 * @created 01.10.2013
+	 *
 	 * @param file the input file to read from
 	 * @return the content of the file
 	 * @throws IOException if the specified file cannot be read completely
+	 * @created 01.10.2013
 	 */
 	public static String getText(File file) throws IOException {
 		return Streams.getTextAndClose(new FileInputStream(file));
@@ -123,24 +123,24 @@ public class Files {
 	}
 
 	/**
-	 * Returns the file extension without the leading ".". If the file has no
-	 * ".", the empty String is returned. if the file is null, null is returned.
-	 * 
-	 * @created 15.02.2014
+	 * Returns the file extension without the leading ".". If the file has no ".", the empty String
+	 * is returned. if the file is null, null is returned.
+	 *
 	 * @param filename the file to get the extension from
 	 * @return the extension of the specified file
+	 * @created 15.02.2014
 	 */
 	public static String getExtension(String filename) {
 		return getExtension(new File(filename));
 	}
 
 	/**
-	 * Returns the file extension without the leading ".". If the file has no
-	 * ".", the empty String is returned. if the file is null, null is returned.
-	 * 
-	 * @created 15.02.2014
+	 * Returns the file extension without the leading ".". If the file has no ".", the empty String
+	 * is returned. if the file is null, null is returned.
+	 *
 	 * @param file the file to get the extension from
 	 * @return the extension of the specified file
+	 * @created 15.02.2014
 	 */
 	public static String getExtension(File file) {
 		if (file == null) return null;
@@ -153,9 +153,39 @@ public class Files {
 	}
 
 	/**
-	 * Recursively gets all files matching the specified {@link FileFilter}.
-	 * If no filter is specified, all files recursively contained in the specified
-	 * directory are returned.
+	 * Returns true if the specified file has one of the specified file extensions. The extensions
+	 * are tested case insensitive.
+	 *
+	 * @param fileName the abstract path of the file to be tested
+	 * @param extensions the extensions to be tested for
+	 * @return if the file has any of the specified extensions
+	 */
+	public static boolean hasExtension(String fileName, String... extensions) {
+		for (String extension : extensions) {
+			if (fileName.length() <= extension.length()) continue;
+			if (Strings.endsWithIgnoreCase(fileName, extension)
+					&& fileName.charAt(fileName.length() - extension.length() - 1) == '.') {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns true if the specified file has one of the specified file extensions. The extensions
+	 * are tested case insensitive.
+	 *
+	 * @param file the file to be tested
+	 * @param extensions the extensions to be tested for
+	 * @return if the file has any of the specified extensions
+	 */
+	public static boolean hasExtension(File file, String... extensions) {
+		return hasExtension(file.getName(), extensions);
+	}
+
+	/**
+	 * Recursively gets all files matching the specified {@link FileFilter}. If no filter is
+	 * specified, all files recursively contained in the specified directory are returned.
 	 *
 	 * @param root the root directory
 	 * @param filter filters the files
@@ -167,11 +197,11 @@ public class Files {
 		for (File f : list) {
 			if (f.isDirectory()) {
 				files.addAll(recursiveGet(f, filter));
-			} else if (filter == null || filter.accept(f)) {
+			}
+			else if (filter == null || filter.accept(f)) {
 				files.add(f);
 			}
 		}
 		return files;
 	}
-
 }
