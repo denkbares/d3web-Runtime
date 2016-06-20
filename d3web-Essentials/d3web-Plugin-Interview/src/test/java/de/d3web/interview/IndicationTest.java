b2d3e055689c7f8d6d98f38849f5b5e37c1c75e7
Albrecht Striffler <albrecht.striffler@denkbares.com>
2016-06-20 16:07:03 +0200

    More indication tests.

--- d3web-Essentials/d3web-Plugin-Interview/src/test/java/de/d3web/interview/IndicationTest.java
+++ d3web-Essentials/d3web-Plugin-Interview/src/test/java/de/d3web/interview/IndicationTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import de.d3web.core.inference.Rule;
 import de.d3web.core.inference.condition.CondEqual;
 import de.d3web.core.inference.condition.Condition;
+import de.d3web.core.knowledge.Indication;
 import de.d3web.core.knowledge.KnowledgeBase;
 import de.d3web.core.knowledge.terminology.QASet;
 import de.d3web.core.knowledge.terminology.QContainer;
@@ -18,15 +19,21 @@ import de.d3web.core.manage.KnowledgeBaseUtils;
 import de.d3web.core.manage.RuleFactory;
 import de.d3web.core.session.Session;
 import de.d3web.core.session.SessionFactory;
+import de.d3web.core.session.Value;
 import de.d3web.core.session.blackboard.Fact;
 import de.d3web.core.session.blackboard.FactFactory;
 import de.d3web.core.session.values.ChoiceValue;
+import de.d3web.core.session.values.Unknown;
 import de.d3web.indication.ActionNextQASet;
 import de.d3web.indication.inference.PSMethodStrategic;
+import de.d3web.indication.inference.PSMethodUserSelected;
 import de.d3web.interview.indication.ActionRepeatedIndication;
 import de.d3web.interview.inference.PSMethodInterview;
 import de.d3web.plugin.test.InitPluginManager;
 
+import static de.d3web.core.knowledge.Indication.State.INDICATED;
+import static de.d3web.core.knowledge.Indication.State.INSTANT_INDICATED;
+import static de.d3web.core.knowledge.Indication.State.RELEVANT;
 import static de.d3web.core.manage.RuleFactory.setRuleParams;
 import static org.junit.Assert.assertEquals;
 
@@ -81,41 +88,102 @@ public class IndicationTest {
 
 	@Test
 	public void testNormalIndication() {
-		testIndication(indicationQuestion1);
+		testIndicationRetraction(indicationQuestion1);
+
+		Session session = SessionFactory.createSession(kb);
+
+		indicate(session, indicationQuestion1, INDICATED);
+		assertActiveQuestions(session, indicationQuestion1);
+		answer(session, indicationQuestion1, Unknown.getInstance());
+		assertActiveQuestions(session);
+		indicate(session, indicationQuestion1, INDICATED);
+		// because the question is already answered, the normal indication will not activate it again
+		assertActiveQuestions(session);
+
+		session = SessionFactory.createSession(kb);
+		indicate(session, indicationContainer, INDICATED);
 	}
 
 	@Test
 	public void testRelevantIndication() {
-		testIndication(indicationQuestion1);
+
+		Session session = SessionFactory.createSession(kb);
+
+		indicate(session, relevantIndicationQuestion, RELEVANT);
+		// without its parent being indicated, relevantQuestion should not be active
+		assertActiveQuestions(session);
+		indicate(session, indicationQuestion1, RELEVANT);
+		// same here, still no active questions
+		assertActiveQuestions(session);
+		indicate(session, indicationContainer, INDICATED);
+		// with its parent indicated, question1 should now be active
+		assertActiveQuestions(session, indicationQuestion1);
+
 	}
 
 	@Test
 	public void testRepeatedIndication() {
-		testIndication(indicationQuestion1);
+		testIndicationRetraction(repeatedIndicationQuestion);
+
+		Session session = SessionFactory.createSession(kb);
+
+		indicate(session, repeatedIndicationQuestion, Indication.State.REPEATED_INDICATED);
+		assertActiveQuestions(session, repeatedIndicationQuestion);
+		answer(session, repeatedIndicationQuestion, Unknown.getInstance());
+		assertActiveQuestions(session);
+		indicate(session, repeatedIndicationQuestion, Indication.State.REPEATED_INDICATED);
+
+		// even if the question is answered already, the repeated indication will activate it again
+		assertActiveQuestions(session, repeatedIndicationQuestion);
+	}
+
+	private void answer(Session session, Question question, Value value) {
+		Fact valueFact = FactFactory.createUserEnteredFact(question, value);
+		session.getBlackboard().addValueFact(valueFact);
+	}
+
+	private void indicate(Session session, QASet qaSet, Indication.State state) {
+		Fact indicationFact = FactFactory.createIndicationFact(qaSet, new Indication(state, kb.getManager()
+						.getTreeIndex(qaSet)),
+				PSMethodUserSelected.getInstance(), PSMethodUserSelected.getInstance());
+		session.getBlackboard().addInterviewFact(indicationFact);
 	}
 
 	@Test
 	public void testInstantIndication() {
-		testIndication(indicationQuestion1);
-	}
+		testIndicationRetraction(instantIndicationQuestion);
 
-	private void testIndication(Question questionToBeIndicated) {
 		Session session = SessionFactory.createSession(kb);
 
-		assertIndication(session);
+		indicate(session, indicationQuestion1, INDICATED);
+		assertActiveQuestions(session, indicationQuestion1);
+
+		// indicating question2 additionally will not change, that question1 was indicated first and is still next
+		indicate(session, indicationQuestion2, INDICATED);
+		assertActiveQuestions(session, indicationQuestion1);
+
+		// instant indicating instantIndicationQuestion will make it the next question, disregarding previous indications
+		indicate(session, instantIndicationQuestion, INSTANT_INDICATED);
+		assertActiveQuestions(session, instantIndicationQuestion);
+	}
+
+	private void testIndicationRetraction(Question questionToBeIndicated) {
+		Session session = SessionFactory.createSession(kb);
+
+		assertActiveQuestions(session);
 
 		Fact userEnteredFact = FactFactory.createUserEnteredFact(indicationChooser, new ChoiceValue(questionToBeIndicated
 				.getName()));
 		session.getBlackboard().addValueFact(userEnteredFact);
 
-		assertIndication(session, questionToBeIndicated);
+		assertActiveQuestions(session, questionToBeIndicated);
 
 		session.getBlackboard().removeValueFact(userEnteredFact);
 
-		assertIndication(session);
+		assertActiveQuestions(session);
 	}
 
-	private void assertIndication(Session session, QASet... qaSetsToIndicate) {
+	private void assertActiveQuestions(Session session, QASet... qaSetsToIndicate) {
 		assertEquals("Content of next form was not as expected", Arrays.asList(qaSetsToIndicate), getNextForm(session).getActiveQuestions());
 	}
 
