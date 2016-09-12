f54a909a4584b25ce0b00e72dfc7b481cea1372a
Albrecht Striffler <albrecht.striffler@denkbares.com>
2016-09-12 18:21:43 +0200

    Added util method to verbalize terminology objects for CI tests.

--- d3web-Essentials/d3web-Plugin-Tests/src/main/java/de/d3web/test/D3webTestUtils.java
+++ d3web-Essentials/d3web-Plugin-Tests/src/main/java/de/d3web/test/D3webTestUtils.java
@@ -23,17 +23,23 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.regex.Pattern;
 
+import com.denkbares.utils.Pair;
 import de.d3web.core.knowledge.TerminologyObject;
+import de.d3web.core.knowledge.terminology.Choice;
 import de.d3web.core.knowledge.terminology.NamedObject;
+import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionChoice;
+import de.d3web.core.knowledge.terminology.info.MMInfo;
+import de.d3web.core.session.Value;
+import de.d3web.core.session.values.ChoiceValue;
 import de.d3web.testing.Message;
 import de.d3web.testing.Message.Type;
 import de.d3web.testing.MessageObject;
 import de.d3web.testing.TestingUtils;
-import com.denkbares.utils.Pair;
 
 /**
  * Some utility methods.
- * 
+ *
  * @author Reinhard Hatko
  * @created 26.03.2013
  */
@@ -42,11 +48,11 @@ public class D3webTestUtils {
 	/**
 	 * Convenience method to create error messages when working with
 	 * {@link TerminologyObject}s.
-	 * 
-	 * @created 06.03.2013
+	 *
 	 * @param erroneousObjects the objects not passed the test
-	 * @param failedMessage the failure message for these objects
+	 * @param failedMessage    the failure message for these objects
 	 * @return an error message for the objects
+	 * @created 06.03.2013
 	 */
 	public static Message createFailure(Collection<? extends NamedObject> erroneousObjects, String failedMessage) {
 		Collection<String> objectNames = new ArrayList<>(erroneousObjects.size());
@@ -58,13 +64,13 @@ public class D3webTestUtils {
 
 	/**
 	 * Filters a list of {@link NamedObject}s.
-	 * 
-	 * @created 16.05.2013
-	 * @param objects the objects to be filtered
-	 * @param ignores derived from ignore parameters of a test
+	 *
+	 * @param objects           the objects to be filtered
+	 * @param ignores           derived from ignore parameters of a test
 	 * @param additionalIgnores additional ignores as given by specific test
-	 *        (e.g. name of the rootQASet)
+	 *                          (e.g. name of the rootQASet)
 	 * @return s the filtered List
+	 * @created 16.05.2013
 	 */
 	public static <T extends NamedObject> Collection<T> filterNamed(Collection<T> objects, String[][] ignores, String... additionalIgnores) {
 		Collection<Pattern> ignorePatterns = TestingUtils.compileIgnores(ignores);
@@ -87,12 +93,12 @@ public class D3webTestUtils {
 	/**
 	 * Creates a failure message for the given kb using the given notification
 	 * text and storing the given terminology objects as message objects.
-	 * 
-	 * @created 16.07.2013
-	 * @param objects the wrongly tested objects
-	 * @param kbName the name of the knowledge base affected
+	 *
+	 * @param objects          the wrongly tested objects
+	 * @param kbName           the name of the knowledge base affected
 	 * @param notificationText the failure message
 	 * @return the constructed failure message object
+	 * @created 16.07.2013
 	 */
 	public static Message createFailure(Collection<TerminologyObject> objects, String kbName, String notificationText) {
 		Message message = new Message(Type.FAILURE, notificationText);
@@ -107,13 +113,13 @@ public class D3webTestUtils {
 
 	/**
 	 * Filters a list of {@link TerminologyObject}s.
-	 * 
-	 * @created 26.03.2013
-	 * @param objects the objects to be filtered
-	 * @param ignores derived from ignore parameters of a test
+	 *
+	 * @param objects           the objects to be filtered
+	 * @param ignores           derived from ignore parameters of a test
 	 * @param additionalIgnores additional ignores as given by specific test
-	 *        (e.g. name of the rootQASet)
+	 *                          (e.g. name of the rootQASet)
 	 * @return s the filtered List
+	 * @created 26.03.2013
 	 */
 	public static Collection<TerminologyObject> filter(Collection<TerminologyObject> objects, String[][] ignores, String... additionalIgnores) {
 		return filterNamed(objects, ignores, additionalIgnores);
@@ -143,11 +149,11 @@ public class D3webTestUtils {
 	/**
 	 * Checks, if one of the parents of object is ignored, based on a list of
 	 * Patterns. Does not check for the object itself!
-	 * 
-	 * @created 25.03.2013
-	 * @param object the TerminologyObject to check
+	 *
+	 * @param object         the TerminologyObject to check
 	 * @param ignorePatterns list of {@link Pattern}s to ignores
 	 * @return true, if the object should be ignored, false otherwise
+	 * @created 25.03.2013
 	 */
 	private static boolean isIgnoredInHierarchy(TerminologyObject object, Collection<Pair<Pattern, Boolean>> ignorePatterns) {
 		for (Pair<Pattern, Boolean> pair : ignorePatterns) {
@@ -163,4 +169,31 @@ public class D3webTestUtils {
 		return pair.getA().matcher(object.getName()).matches();
 	}
 
+	/**
+	 * A simple verbalization for terminology objects. Uses prompts, if available.
+	 *
+	 * @param object the object we want to verbalize
+	 * @return the verbalization for the object
+	 */
+	public static String getVerbalization(TerminologyObject object) {
+		String objectPrompt = object.getInfoStore().getValue(MMInfo.PROMPT);
+		return objectPrompt == null ? object.getName() : objectPrompt + " (id: " + object.getName() + ")";
+	}
+
+	/**
+	 * A simple verbalization for values. If question is a {@link QuestionChoice} and value is a {@link ChoiceValue}, we
+	 * try to use the prompt of the choice.
+	 *
+	 * @param question the question belonging to the value
+	 * @param value    the value we want to verbalize
+	 * @return the verbalization for the object
+	 */
+	public static String getVerbalization(Question question, Value value) {
+		if (!(question instanceof QuestionChoice && value instanceof ChoiceValue)) {
+			return value.getValue().toString();
+		}
+		Choice choice = ((ChoiceValue) value).getChoice((QuestionChoice) question);
+		String choicePrompt = choice == null ? null : choice.getInfoStore().getValue(MMInfo.PROMPT);
+		return choicePrompt == null ? value.getValue().toString() : choicePrompt + " (id: " + choice.getName() + ")";
+	}
 }
