34c934a8fe1b40782607413d17ba33e48ab6d160
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-01-10 17:58:52 +0100

    Persistence now also reads and writes object of NOOPAction.

--- d3web-Plugins/d3web-Plugin-DiaFlux/src/main/java/de/d3web/diaFlux/io/NOOPActionHandler.java
+++ d3web-Plugins/d3web-Plugin-DiaFlux/src/main/java/de/d3web/diaFlux/io/NOOPActionHandler.java
@@ -19,6 +19,7 @@
 package de.d3web.diaFlux.io;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.w3c.dom.Element;
 
@@ -26,27 +27,51 @@ import de.d3web.core.io.Persistence;
 import de.d3web.core.io.fragments.FragmentHandler;
 import de.d3web.core.io.utilities.XMLUtil;
 import de.d3web.core.knowledge.KnowledgeBase;
+import de.d3web.core.knowledge.TerminologyObject;
 import de.d3web.diaFlux.flow.NOOPAction;
 
 /**
- * 
  * @author Reinhard Hatko
  * @created 11.01.2011
  */
 public class NOOPActionHandler implements FragmentHandler<KnowledgeBase> {
 
 	private static final String NOOP = "NOOP";
+	private static final String NOOP_OBJECT = "Object";
 
 	@Override
 	public Object read(Element element, Persistence<KnowledgeBase> persistence) throws IOException {
-		return new NOOPAction();
+		List<Element> children = XMLUtil.getChildren(element);
+		if (children.size() > 1) {
+			throw new IOException("Cannot handle NOOPAction with more than one forward object");
+		}
+		if (children.isEmpty()) {
+			return new NOOPAction();
+		}
+		else {
+			Element objectElement = children.get(0);
+			String name = objectElement.getAttribute("name");
+			TerminologyObject object = persistence.getArtifact().getManager().search(name);
+			if (object == null) {
+				throw new IOException("Object with name '" + name + " not found (trying to create NOOPAction)");
+			}
+			return new NOOPAction(object);
+		}
 	}
 
 	@Override
 	public Element write(Object object, Persistence<KnowledgeBase> persistence) throws IOException {
 		Element actionElem = persistence.getDocument().createElement(ActionNodeFragmentHandler.ACTION);
 		actionElem.setAttribute("type", NOOP);
-
+		List<? extends TerminologyObject> forwardObjects = ((NOOPAction) object).getForwardObjects();
+		if (forwardObjects.size() > 1) {
+			throw new IOException("Cannot handle NOOPAction with more than one forward object");
+		}
+		else if (forwardObjects.size() == 1) {
+			Element element = persistence.getDocument().createElement(NOOP_OBJECT);
+			element.setAttribute("name", forwardObjects.get(0).getName());
+			actionElem.appendChild(element);
+		}
 		return actionElem;
 	}
 
