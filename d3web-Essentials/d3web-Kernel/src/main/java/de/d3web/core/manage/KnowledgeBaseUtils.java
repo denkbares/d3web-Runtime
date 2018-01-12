b6f7b9eb415b08f3d9b03c899b5e746bf8ae8c6f
Jonas Müller <jonas.mueller@compitencies.com>
2018-01-12 16:48:19 +0100

    First version of XPS printing
    * After having started an XPS session a print button appears
    * A custom document will be shown in the browser's print dialogue
    * The document can be styled individually by putting a 'xps-print.xls' file in the datafolder's branding folder
    * Otherwise a general XLST template and stylings will be used

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/manage/KnowledgeBaseUtils.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/manage/KnowledgeBaseUtils.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -660,11 +660,15 @@ public final class KnowledgeBaseUtils {
 		for (State state : solutionStates) {
 			List<Solution> solutions = session.getBlackboard().getSolutions(state);
 			solutions.sort(new SolutionComparator(session));
-			for (Solution solution : solutions) {
-				groupSolution(solution, groups);
-			}
+			groupSolutions(groups, solutions);
 		}
 		return groups;
+	}
+
+	public static void groupSolutions(MultiMap<Solution, Solution> groups, List<Solution> solutions) {
+		for (Solution solution : solutions) {
+			groupSolution(solution, groups);
+		}
 	}
 
 	private static void groupSolution(Solution solution, MultiMap<Solution, Solution> groups) {
@@ -730,7 +734,8 @@ public final class KnowledgeBaseUtils {
 				session.getProtocol().addEntry(new FactProtocolEntry(
 						session.getPropagationManager().getPropagationTime(), fact));
 			}
-		} finally {
+		}
+		finally {
 			session.getPropagationManager().commitPropagation();
 		}
 	}
