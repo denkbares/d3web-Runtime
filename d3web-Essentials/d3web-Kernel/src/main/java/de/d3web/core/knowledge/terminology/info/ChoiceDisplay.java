69263942c0ce20c750848997a71eda6c7b03af34
volker.belli <volker.belli@denkbares.com>
2018-02-07 17:22:25 +0100

    added checklist as new choice display

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/info/ChoiceDisplay.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/info/ChoiceDisplay.java
@@ -12,8 +12,9 @@ package de.d3web.core.knowledge.terminology.info;
  */
 public enum ChoiceDisplay {
 	/**
-	 * Display the value as a normal (plain) value to the user. If the value is edible, the client application decide
-	 * how the value is displayed, usually as buttons (or similar) to be clicked
+	 * Display the value as a normal (plain) value to the user. If the value is editable, the client application decide
+	 * how the value is displayed, usually as buttons (or similar) to be clicked. If not editable, usually only the
+	 * selected choice(s) are displayed.
 	 */
 	normal,
 
@@ -26,5 +27,12 @@ public enum ChoiceDisplay {
 	 * Displays the choices as e.g. a drop-down field, often combined with some ability to enter text to filter the
 	 * potential values during typing. This option may e.g. be used for large numbers of potential answer values.
 	 */
-	filter
+	filter,
+
+	/**
+	 * Displays the choices as a checklist, with checked entries for each selected choice. Mostly useful for mc
+	 * questions. Especially if the question is abstract (read-only), the implementer of the user interface should
+	 * ensure that all choices are displayed, even is not selected (yet).
+	 */
+	checklist
 }
