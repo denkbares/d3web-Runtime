23fac2c57087594c6852bb8c82cf74724d23f9b2
volker.belli <volker.belli@denkbares.com>
2018-02-13 18:56:07 +0100

    fixed potential NPE

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/session/protocol/DefaultProtocol.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/session/protocol/DefaultProtocol.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2011 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -28,7 +28,7 @@ import java.util.Objects;
 
 /**
  * Default implementation of Protocol
- * 
+ *
  * @author Joachim Baumeister (denkbares GmbH)
  */
 public class DefaultProtocol implements Protocol {
@@ -69,6 +69,7 @@ public class DefaultProtocol implements Protocol {
 	@Override
 	public void addEntries(Collection<? extends ProtocolEntry> entries) {
 		for (ProtocolEntry entry : entries) {
+			if (entry == null) continue;
 			addEntry(entry);
 		}
 	}
@@ -87,5 +88,4 @@ public class DefaultProtocol implements Protocol {
 	public boolean removeEntry(ProtocolEntry entry) {
 		return entries.remove(entry);
 	}
-
 }
