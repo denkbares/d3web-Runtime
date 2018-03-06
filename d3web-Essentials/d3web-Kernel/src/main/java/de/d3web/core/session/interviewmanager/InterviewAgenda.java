b03c8cb0a50a62a36257594cadf607e610adff5c
Albrecht Striffler <albrecht.striffler@denkbares.com>
2018-03-06 18:45:29 +0100

    Minor cleanup.

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/session/interviewmanager/InterviewAgenda.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/session/interviewmanager/InterviewAgenda.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -21,7 +21,6 @@ package de.d3web.core.session.interviewmanager;
 import de.d3web.core.knowledge.InterviewObject;
 
 /**
- * 
  * @author Markus Friedrich (denkbares GmbH)
  * @created 17.06.2013
  */
@@ -32,5 +31,4 @@ public interface InterviewAgenda {
 	}
 
 	boolean hasState(InterviewObject interviewObject, InterviewState state);
-
 }
