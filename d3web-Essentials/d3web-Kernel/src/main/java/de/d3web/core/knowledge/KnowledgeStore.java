e62f00d08395d12f788235167025d014c635d2c0
volker.belli <volker.belli@denkbares.com>
2018-11-29 09:39:14 +0100

    added "computeIfAbsent" to knowledge store, so access a knowledge slice, adding a default value if not available.

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/KnowledgeStore.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/KnowledgeStore.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2010 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,68 +20,85 @@
 
 package de.d3web.core.knowledge;
 
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import de.d3web.core.inference.KnowledgeKind;
 import de.d3web.core.inference.KnowledgeSlice;
 import de.d3web.core.inference.RuleSet;
 
 /**
- * The problem-solving knowledge of the knowledge base is managed in
- * {@link KnowledgeStore} instances. Every {@link TerminologyObject} instance
- * then stores all knowledge that corresponds to the {@link TerminologyObject}
- * instance. Possible knowledge elements can be {@link RuleSet} instances or
- * set-covering models.
- * 
+ * The problem-solving knowledge of the knowledge base is managed in {@link KnowledgeStore} instances. Every {@link
+ * TerminologyObject} instance then stores all knowledge that corresponds to the {@link TerminologyObject} instance.
+ * Possible knowledge elements can be {@link RuleSet} instances or set-covering models.
+ *
  * @author Markus Friedrich (denkbares GmbH)
  * @created 06.05.2011
  */
 public interface KnowledgeStore {
 
 	/**
-	 * Inserts the specified {@link KnowledgeSlice} instance with the specified
-	 * {@link KnowledgeKind} to the store. {@link KnowledgeKind} instances
-	 * correspond to the particular problem-solving methods and the
-	 * {@link KnowledgeSlice} instances are concrete knowledge elements that can
-	 * be processed by the related problem-solving method.
-	 * 
-	 * @created 06.05.2011
-	 * @param kind the specified {@link KnowledgeKind}
+	 * Inserts the specified {@link KnowledgeSlice} instance with the specified {@link KnowledgeKind} to the store.
+	 * {@link KnowledgeKind} instances correspond to the particular problem-solving methods and the {@link
+	 * KnowledgeSlice} instances are concrete knowledge elements that can be processed by the related problem-solving
+	 * method.
+	 *
+	 * @param kind  the specified {@link KnowledgeKind}
 	 * @param slice the specified knowledge element
+	 * @created 06.05.2011
 	 */
 	void addKnowledge(KnowledgeKind<?> kind, KnowledgeSlice slice);
 
 	/**
-	 * Removes the specified {@link KnowledgeSlice} instance with the specified
-	 * {@link KnowledgeKind} from the store. {@link KnowledgeKind} instances
-	 * correspond to the particular problem-solving methods and the
-	 * {@link KnowledgeSlice} instances are concrete knowledge elements that can
-	 * be processed by the related problem-solving method.
-	 * 
-	 * @created 06.05.2011
-	 * @param kind the specified {@link KnowledgeKind}
+	 * Removes the specified {@link KnowledgeSlice} instance with the specified {@link KnowledgeKind} from the store.
+	 * {@link KnowledgeKind} instances correspond to the particular problem-solving methods and the {@link
+	 * KnowledgeSlice} instances are concrete knowledge elements that can be processed by the related problem-solving
+	 * method.
+	 *
+	 * @param kind  the specified {@link KnowledgeKind}
 	 * @param slice the specified knowledge element
+	 * @created 06.05.2011
 	 */
 	void removeKnowledge(KnowledgeKind<?> kind, KnowledgeSlice slice);
 
 	/**
-	 * Returns all {@link KnowledgeSlice} instances included in this
-	 * {@link KnowledgeStore} that are connected with the specified
-	 * {@link KnowledgeKind}.
-	 * 
-	 * @created 06.05.2011
+	 * Returns the {@link KnowledgeSlice} included in this {@link KnowledgeStore} that is connected with the specified
+	 * {@link KnowledgeKind}. If there is no such KnowledgeSlice, null is returned.
+	 *
 	 * @param kind the specified {@link KnowledgeKind}
-	 * @return all knowledge elements related to the specified
-	 *         {@link KnowledgeKind}
+	 * @return the knowledge related to the specified {@link KnowledgeKind}
+	 * @created 06.05.2011
 	 */
-	<T extends KnowledgeSlice> T getKnowledge(KnowledgeKind<T> kind);
+	@Nullable <T extends KnowledgeSlice> T getKnowledge(KnowledgeKind<T> kind);
 
 	/**
-	 * Returns all knowledge elements for all registered {@link KnowledgeKind},
-	 * that are included in this {@link KnowledgeStore} instance.
-	 * 
+	 * Returns the {@link KnowledgeSlice} included in this {@link KnowledgeStore} that is connected with the specified
+	 * {@link KnowledgeKind}. If there is no such KnowledgeSlice, a new one is created, using the specified supplier,
+	 * and added to this store. The supplier must not return null, otherwise a {@link NullPointerException} is thrown.
+	 *
+	 * @param kind     the specified {@link KnowledgeKind}
+	 * @param supplier a supplier to create a new instance if there is no such knowledge in this store
+	 * @return the knowledge related to the specified {@link KnowledgeKind}
+	 */
+	@NotNull
+	default <T extends KnowledgeSlice> T computeIfAbsent(KnowledgeKind<T> kind, Supplier<? extends T> supplier) {
+		T knowledge = getKnowledge(kind);
+		if (knowledge == null) {
+			knowledge = Objects.requireNonNull(supplier.get());
+			addKnowledge(kind, knowledge);
+		}
+		return knowledge;
+	}
+
+	/**
+	 * Returns all knowledge elements for all registered {@link KnowledgeKind}, that are included in this {@link
+	 * KnowledgeStore} instance.
+	 *
+	 * @return all knowledge elements no matter what the particular {@link KnowledgeKind} is
 	 * @created 06.05.2011
-	 * @return all knowledge elements no matter what the particular
-	 *         {@link KnowledgeKind} is
 	 */
 	KnowledgeSlice[] getKnowledge();
-
 }
