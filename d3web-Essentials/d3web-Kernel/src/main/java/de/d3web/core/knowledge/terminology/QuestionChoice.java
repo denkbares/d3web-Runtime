d7abe371c332212ff3f72255c6fe7a2a94749fe7
Albrecht Striffler <albrecht.striffler@denkbares.com>
2020-05-19 11:34:14 +0200

    Add @NotNull for getAllAlternatives() and some cleanup

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/QuestionChoice.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/QuestionChoice.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -24,6 +24,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.jetbrains.annotations.NotNull;
+
 import de.d3web.core.knowledge.KnowledgeBase;
 import de.d3web.core.manage.KnowledgeBaseUtils;
 
@@ -31,7 +33,7 @@ import de.d3web.core.manage.KnowledgeBaseUtils;
  * Storage for Questions with predefined answers (alternatives). Abstract
  * because you can choose from multiple/single choices (answers).<BR>
  * Part of the Composite design pattern (see QASet for further description)
- * 
+ *
  * @author joba, Christian Betz
  * @see QASet
  */
@@ -58,10 +60,11 @@ public abstract class QuestionChoice extends Question {
 	/**
 	 * Gives you all the answers (alternatives) and does not care about any
 	 * rules which could possibly suppress an answer.
-	 * 
+	 *
 	 * @return a List of all alternatives that are not suppressed by any
-	 *         RuleSuppress
+	 * RuleSuppress
 	 **/
+	@NotNull
 	public List<Choice> getAllAlternatives() {
 		return Collections.unmodifiableList(alternatives);
 	}
@@ -86,7 +89,6 @@ public abstract class QuestionChoice extends Question {
 				this.alternatives.add(choice);
 			}
 		}
-
 	}
 
 	public void addAlternative(Choice answer) {
@@ -98,7 +100,7 @@ public abstract class QuestionChoice extends Question {
 
 	public void addAlternative(Choice answer, int pos) {
 		if (answer != null && !hasChoice(answer)) {
-			alternatives.add(pos > alternatives.size() ? alternatives.size() : pos, answer);
+			alternatives.add(Math.min(pos, alternatives.size()), answer);
 			answer.setQuestion(this);
 		}
 	}
