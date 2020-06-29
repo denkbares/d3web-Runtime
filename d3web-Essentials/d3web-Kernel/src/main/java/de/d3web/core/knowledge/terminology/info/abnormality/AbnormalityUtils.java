4fe4ac0b82247e87eeefaf90ac620ccfd4ec6a6c
Veronika Sehne <Veronika Sehne>
2020-06-29 12:11:31 +0200

    Add OC questions for optional activities [CBX-254]

--- d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/info/abnormality/AbnormalityUtils.java
+++ d3web-Essentials/d3web-Kernel/src/main/java/de/d3web/core/knowledge/terminology/info/abnormality/AbnormalityUtils.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2009 Chair of Artificial Intelligence and Applied Informatics
  * Computer Science VI, University of Wuerzburg
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,12 +20,15 @@
 
 package de.d3web.core.knowledge.terminology.info.abnormality;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.denkbares.strings.Strings;
 import de.d3web.core.knowledge.terminology.Choice;
 import de.d3web.core.knowledge.terminology.Question;
+import de.d3web.core.knowledge.terminology.QuestionChoice;
 import de.d3web.core.knowledge.terminology.QuestionMC;
 import de.d3web.core.knowledge.terminology.QuestionNum;
 import de.d3web.core.knowledge.terminology.info.BasicProperties;
@@ -81,6 +84,22 @@ public class AbnormalityUtils {
 		}
 
 		return normality;
+	}
+
+	/**
+	 * Gets all answers with abnormality A0 of a question.
+	 * @param question the question to get the normal answers for
+	 * @return the normal answers of a question
+	 */
+	public static List<Choice> getNormalAnswers(QuestionChoice question) {
+		Abnormality abnormality = BasicProperties.getAbnormality(question);
+		List<Choice> normalAnswers = new ArrayList<>();
+		for (Choice choice : question.getAllAlternatives()) {
+			if (abnormality.getValue(new ChoiceValue(choice)) == A0) {
+				normalAnswers.add(choice);
+			}
+		}
+		return normalAnswers;
 	}
 
 	/**
