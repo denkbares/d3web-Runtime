b1865e0a14ca25988d27bf55922d49ac3d629689
Volker Belli <volker.belli@denkbares.com>
2021-03-11 13:07:06 +0100

    ix type

--- d3web-Essentials/d3web-Persistence/src/main/java/de/d3web/core/io/KnowledgeReader.java
+++ d3web-Essentials/d3web-Persistence/src/main/java/de/d3web/core/io/KnowledgeReader.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2009 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -26,25 +26,19 @@ import de.d3web.core.knowledge.KnowledgeBase;
 
 /**
  * Interface for all reading persistence handlers.
- * 
+ *
  * @author Markus Friedrich (denkbares GmbH)
  */
 public interface KnowledgeReader {
 
 	/**
-	 * This method reads the input stream and inserts the knowledge in the
-	 * {@link KnowledgeBase} instance.
-	 * 
+	 * This method reads the input stream and inserts the knowledge in the {@link KnowledgeBase} instance.
+	 *
 	 * @param persistenceManager the persistence manager responsible for reading
-	 * @param knowledgeBase KnowledgeBase in which die knowledge should be
-	 *        inserted
-	 * @param stream InputStream containing the knowledge
-	 * @param listerner listener which will be informed during this operation,
-	 *        null is not accepted
-	 * 
-	 * @throws IOException when an IO errors occurs during reading the part of
-	 *         the knowledge base
+	 * @param knowledgeBase      KnowledgeBase in which die knowledge should be inserted
+	 * @param stream             InputStream containing the knowledge
+	 * @param progress           progress listener which will be informed during this operation, null is not accepted
+	 * @throws IOException when an IO errors occurs during reading the part of the knowledge base
 	 */
-	void read(PersistenceManager persistenceManager, KnowledgeBase knowledgeBase, InputStream stream, ProgressListener listerner) throws IOException;
-
+	void read(PersistenceManager persistenceManager, KnowledgeBase knowledgeBase, InputStream stream, ProgressListener progress) throws IOException;
 }
--- d3web-Essentials/d3web-Persistence/src/main/java/de/d3web/core/io/KnowledgeWriter.java
+++ d3web-Essentials/d3web-Persistence/src/main/java/de/d3web/core/io/KnowledgeWriter.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2009 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -32,26 +32,22 @@ import de.d3web.core.knowledge.KnowledgeBase;
 public interface KnowledgeWriter {
 
 	/**
-	 * Writes the parts of the specified {@link KnowledgeBase} instance, that
-	 * this KnowledgeWriter can handle, into the {@link OutputStream}.
+	 * Writes the parts of the specified {@link KnowledgeBase} instance, that this KnowledgeWriter can handle, into the
+	 * {@link OutputStream}.
 	 *
 	 * @param persistenceManager the persistence manager responsible for writing
-	 * @param knowledgeBase      the specified knowledge base in which the handled
-	 *                           knowledge is included
-	 * @param stream             the specified {@link OutputStream} to which the handled
-	 *                           knowledge is written
-	 * @param listener           the specified listener which will be notified during this
-	 *                           operation, null is not accepted
+	 * @param knowledgeBase      the specified knowledge base in which the handled knowledge is included
+	 * @param stream             the specified {@link OutputStream} to which the handled knowledge is written
+	 * @param progress           the specified listener which will be notified during this operation, null is not
+	 *                           accepted
 	 * @throws IOException when an IO exception occurs during the write action
 	 */
-	void write(PersistenceManager persistenceManager, KnowledgeBase knowledgeBase, OutputStream stream, ProgressListener listener) throws IOException;
+	void write(PersistenceManager persistenceManager, KnowledgeBase knowledgeBase, OutputStream stream, ProgressListener progress) throws IOException;
 
 	/**
-	 * The size of the knowledge--written by this {@link KnowledgeWriter}--is
-	 * valued by this method.
+	 * The size of the knowledge--written by this {@link KnowledgeWriter}--is valued by this method.
 	 *
-	 * @param knowledgeBase knowledge base containing the knowledge under
-	 *                      estimation
+	 * @param knowledgeBase knowledge base containing the knowledge under estimation
 	 * @return the valued size of the write method
 	 */
 	default int getEstimatedSize(KnowledgeBase knowledgeBase) {
